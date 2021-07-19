8119dbc1e460a2e064a4a05384d3c6f56f4b740a
David Colburn <xero73@gmail.com>
2021-07-19 15:07:43 -0700

    move hidden

--- auth/grants.go
+++ auth/grants.go
@@ -9,12 +9,14 @@ type VideoGrant struct {
 	RoomAdmin bool   `json:"roomAdmin,omitempty"`
 	RoomJoin  bool   `json:"roomJoin,omitempty"`
 	Room      string `json:"room,omitempty"`
-	Hidden    bool   `json:"hidden,omitempty"`
 
 	// permissions within a room, if none of the permissions are set
 	// it's interpreted as both are permissible
 	CanPublish   bool `json:"canPublish,omitempty"`
 	CanSubscribe bool `json:"canSubscribe,omitempty"`
+
+	// used for recording
+	Hidden bool `json:"hidden,omitempty"`
 }
 
 type ClaimGrants struct {
