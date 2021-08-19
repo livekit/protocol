d60d683ac8bf086818263ccb368b498ea8d72a46
David Zhao <david@davidzhao.com>
2021-08-18 22:47:47 -0700

    Use pointers for perms in order to allow nil (#11)
    * nullable permission grants
    
    * nil perms, canPublishData
    
    * convenience setters

--- auth/grants.go
+++ auth/grants.go
@@ -13,8 +13,9 @@ type VideoGrant struct {
 
 	// permissions within a room, if none of the permissions are set
 	// it's interpreted as both are permissible
-	CanPublish   bool `json:"canPublish,omitempty"`
-	CanSubscribe bool `json:"canSubscribe,omitempty"`
+	CanPublish     *bool `json:"canPublish,omitempty"`
+	CanSubscribe   *bool `json:"canSubscribe,omitempty"`
+	CanPublishData *bool `json:"canPublishData,omitempty"`
 
 	// used for recording
 	Hidden bool `json:"hidden,omitempty"`
@@ -26,4 +27,16 @@ type ClaimGrants struct {
 	// for verifying integrity of the message body
 	Sha256   string `json:"sha256,omitempty"`
 	Metadata string `json:"metadata,omitempty"`
+}
+
+func (v *VideoGrant) SetCanPublish(val bool) {
+	v.CanPublish = &val
+}
+
+func (v *VideoGrant) SetCanPublishData(val bool) {
+	v.CanPublishData = &val
+}
+
+func (v *VideoGrant) SetCanSubscribe(val bool) {
+	v.CanSubscribe = &val
 }
