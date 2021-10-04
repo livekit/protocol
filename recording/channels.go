dd31c5c6a7c9aae11c62d5bad2693680d57775ed
David Colburn <xero73@gmail.com>
2021-10-04 13:20:57 -0700

    recording rpc

--- recording/channels.go
+++ recording/channels.go
@@ -1,11 +1,20 @@
 package recording
 
-import "time"
+import (
+	"context"
+	"errors"
+	"time"
+
+	"google.golang.org/protobuf/proto"
+
+	livekit "github.com/livekit/protocol/proto"
+	"github.com/livekit/protocol/utils"
+)
 
 const (
 	ReservationChannel = "RESERVE_RECORDER"
 	ResultChannel      = "RECORDING_RESULT"
-	RecorderTimeout    = time.Second * 3
+	RequestTimeout     = time.Second * 3
 	ReservationTimeout = time.Second * 2
 )
 
@@ -15,4 +24,42 @@ func RequestChannel(id string) string {
 
 func ResponseChannel(id string) string {
 	return "RECORDING_RESPONSE_" + id
+}
+
+func RecordingRPC(ctx context.Context, mb utils.MessageBus, recordingId string, req *livekit.RecordingRequest) error {
+	sub, err := mb.Subscribe(ctx, ResponseChannel(recordingId))
+	if err != nil {
+		return err
+	}
+
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	err = mb.Publish(ctx, RequestChannel(recordingId), b)
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-time.After(RequestTimeout):
+			return errors.New("request timeout")
+		case msg := <-sub.Channel():
+			b = sub.Payload(msg)
+			resp := &livekit.RecordingResponse{}
+			err = proto.Unmarshal(b, resp)
+			if err != nil {
+				return err
+			}
+			if resp.RequestId != req.RequestId {
+				continue
+			}
+			if !resp.Success {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}
+	}
 }
