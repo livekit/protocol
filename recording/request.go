1dadf893095928a27ae47b63cdce74607c38f229
David Colburn <xero73@gmail.com>
2021-10-06 21:36:55 -0700

    recording v2 (#28)
    * recording v2
    
    * input oneof
    
    * input.input_url -> input.url
    
    * update channels
    
    * simplify reservation
    
    * recording request
    
    * recording response
    
    * recording rpc
    
    * add reserve func
    
    * close subscriber
    
    * file option
    
    * remove scaling

---
+++ recording/request.go
@@ -0,0 +1,93 @@
+package recording
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"google.golang.org/protobuf/proto"
+
+	livekit "github.com/livekit/protocol/proto"
+	"github.com/livekit/protocol/utils"
+)
+
+const (
+	ReservationChannel = "RESERVE_RECORDER"
+	ResultChannel      = "RECORDING_RESULT"
+	RequestTimeout     = time.Second * 3
+	ReservationTimeout = time.Second * 2
+)
+
+func RequestChannel(id string) string {
+	return "RECORDING_REQUEST_" + id
+}
+
+func ResponseChannel(id string) string {
+	return "RECORDING_RESPONSE_" + id
+}
+
+func ReserveRecorder(recordingId string, bus utils.MessageBus) error {
+	req := &livekit.RecordingReservation{
+		Id:          recordingId,
+		SubmittedAt: time.Now().UnixNano() / 1e6,
+	}
+
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	sub, _ := bus.Subscribe(context.Background(), ResponseChannel(recordingId))
+	defer sub.Close()
+
+	err = bus.Publish(context.Background(), ReservationChannel, string(b))
+	if err != nil {
+		return err
+	}
+
+	select {
+	case <-sub.Channel():
+		return nil
+	case <-time.After(RequestTimeout):
+		return errors.New("no recorders available")
+	}
+}
+
+func RPC(ctx context.Context, bus utils.MessageBus, recordingId string, req *livekit.RecordingRequest) error {
+	sub, err := bus.Subscribe(ctx, ResponseChannel(recordingId))
+	if err != nil {
+		return err
+	}
+	defer sub.Close()
+
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	err = bus.Publish(ctx, RequestChannel(recordingId), b)
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-time.After(RequestTimeout):
+			return errors.New("request timeout")
+		case msg := <-sub.Channel():
+			b = sub.Payload(msg)
+			resp := &livekit.RecordingResponse{}
+			err = proto.Unmarshal(b, resp)
+			if err != nil {
+				return err
+			}
+			if resp.RequestId != req.RequestId {
+				continue
+			}
+			if !resp.Success {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}
+	}
+}
