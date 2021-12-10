a309153bb064e2156774955bb62d36f171e7d923
Mathew Kamkar <578302+matkam@users.noreply.github.com>
2021-12-10 15:20:55 -0800

    move node id generation into this repo

--- utils/id.go
+++ utils/id.go
@@ -1,6 +1,11 @@
 package utils
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"os"
+
+	"github.com/jxskiss/base62"
 	"github.com/lithammer/shortuuid/v3"
 )
 
@@ -16,4 +21,21 @@ const (
 
 func NewGuid(prefix string) string {
 	return prefix + shortuuid.New()[:12]
+}
+
+// Creates a hashed ID from a unique string
+func HashedID(id string) string {
+	h := sha1.New()
+	h.Write([]byte(id))
+	val := h.Sum(nil)
+
+	return base62.EncodeToString(val)
+}
+
+func LocalNodeID() (string, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s%s", NodePrefix, HashedID(hostname)[:8]), nil
 }
