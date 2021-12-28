88ab66e0e761ff304042b286c41de3c803b45576
Raja Subramanian <raja.gobi@tutanota.com>
2021-12-28 14:18:26 +0530

    LiveKit type aliases (#62)
    * LiveKit type aliases
    
    Idea is to start with aliases so that this can be done in small steps.
    The plan is to move to a type definition once we have most of the stuff
    moved over to get stronger type checks.
    
    * Sid -> ID

---
+++ livekit/types.go
@@ -0,0 +1,10 @@
+package livekit
+
+type TrackID = string
+
+type ParticipantID = string
+type ParticipantIdentity = string
+type ParticipantName = string
+
+type RoomID = string
+type RoomName = string
