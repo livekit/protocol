aed267232cdacf382a6b3867cbde2bb7022f71ac
boks1971 <raja.gobi@tutanota.com>
2021-12-31 16:13:53 +0530

    Make room name a type definition

--- livekit/types.go
+++ livekit/types.go
@@ -7,4 +7,27 @@ type ParticipantIdentity = string
 type ParticipantName = string
 
 type RoomID = string
-type RoomName = string
+
+//----------------------------------------------------------------
+
+type RoomName string
+
+func RoomNamesAsStrings(roomNames []RoomName) []string {
+	var asString []string
+	for _, roomName := range roomNames {
+		asString = append(asString, string(roomName))
+	}
+
+	return asString
+}
+
+func StringsAsRoomNames(roomNames []string) []RoomName {
+	var asRoomName []RoomName
+	for _, roomName := range roomNames {
+		asRoomName = append(asRoomName, RoomName(roomName))
+	}
+
+	return asRoomName
+}
+
+//----------------------------------------------------------------
