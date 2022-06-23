487454ca5ff27f7ddb83226c4f045f8d80bb4897
David Colburn <xero73@gmail.com>
2022-06-23 14:13:55 -0700

    Tracing

---
+++ tracer/tracer.go
@@ -0,0 +1,32 @@
+package tracer
+
+import "context"
+
+type Tracer interface {
+	Start(ctx context.Context, spanName string, opts ...interface{}) (context.Context, Span)
+}
+
+type Span interface {
+	End()
+}
+
+var tracer Tracer = &NoOpTracer{}
+
+// Can be used for your own tracing (for example, with Lightstep)
+func SetTracer(t Tracer) {
+	tracer = t
+}
+
+func Start(ctx context.Context, spanName string, opts ...interface{}) (context.Context, Span) {
+	return tracer.Start(ctx, spanName, opts...)
+}
+
+type NoOpTracer struct{}
+
+func (t *NoOpTracer) Start(ctx context.Context, _ string, _ ...interface{}) (context.Context, Span) {
+	return ctx, &NoOpSpan{}
+}
+
+type NoOpSpan struct{}
+
+func (s *NoOpSpan) End() {}
