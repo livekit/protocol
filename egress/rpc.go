8c070370289f7d6fe4b87adfd89d9fcc3c9871eb
Benjamin Pracht <benjamin@livekit.io>
2022-07-09 17:06:49 -0700

    Make sure we do not override the egressid when forwarding messages from the controller

--- egress/rpc.go
+++ egress/rpc.go
@@ -73,7 +73,9 @@ func (r *RedisRPC) SendRequest(ctx context.Context, request proto.Message) (*liv
 
 	switch req := request.(type) {
 	case *livekit.StartEgressRequest:
-		req.EgressId = utils.NewGuid(utils.EgressPrefix)
+		if req.EgressId == "" {
+			req.EgressId = utils.NewGuid(utils.EgressPrefix)
+		}
 		req.RequestId = requestID
 		req.SentAt = time.Now().UnixNano()
 		req.SenderId = string(r.nodeID)
