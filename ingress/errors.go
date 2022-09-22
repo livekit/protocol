19ead11853f17e66339438f16b9f7ab8e2d28dad
Benjamin Pracht <benjamin@livekit.io>
2022-09-22 13:44:13 -0700

    Add ErrNoResponse to ingress (#213)

--- ingress/errors.go
+++ ingress/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrIngressOutOfDate = errors.New("trying to ovewrite an ingress with an older version")
 	ErrIngressTimedOut  = errors.New("ingress timed out")
+	ErrNoResponse       = errors.New("no response from ingress service")
 )
