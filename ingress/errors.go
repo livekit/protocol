f69b436be81ea5f67a6264a608bf4e359d78b613
Benjamin Pracht <benjamin@livekit.io>
2022-10-20 09:24:42 -0700

    Move video output validation to protocol (#233)

--- ingress/errors.go
+++ ingress/errors.go
@@ -3,7 +3,18 @@ package ingress
 import "errors"
 
 var (
-	ErrIngressOutOfDate = errors.New("trying to ovewrite an ingress with an older version")
-	ErrIngressTimedOut  = errors.New("ingress timed out")
-	ErrNoResponse       = errors.New("no response from ingress service")
+	ErrIngressOutOfDate        = errors.New("trying to ovewrite an ingress with an older version")
+	ErrIngressTimedOut         = errors.New("ingress timed out")
+	ErrNoResponse              = errors.New("no response from ingress service")
+	ErrInvalidOutputDimensions = NewInvalidVideoParamsError("invalid output media dimensions")
 )
+
+type InvalidVideoParamsError string
+
+func NewInvalidVideoParamsError(s string) InvalidVideoParamsError {
+	return InvalidVideoParamsError(s)
+}
+
+func (s InvalidVideoParamsError) Error() string {
+	return "invalid video parameters: " + string(s)
+}
