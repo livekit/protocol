9b38d94c3a303cc6bb41bc1e1c0a30d43415ad2d
David Zhao <dz@livekit.io>
2022-10-27 23:11:24 -0700

    Configurable logger sampling parameters

--- logger/config.go
+++ logger/config.go
@@ -4,4 +4,8 @@ type Config struct {
 	JSON   bool   `yaml:"json"`
 	Level  string `yaml:"level"`
 	Sample bool   `yaml:"sample,omitempty"`
+	// when sampling, the first N logs will be logged
+	SampleInitial int `yaml:"sampleInitial,omitempty"`
+	// when sampling, every Mth log will be logged
+	SampleInterval int `yaml:"sampleInitial,omitempty"`
 }
