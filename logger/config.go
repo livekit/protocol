e443a7237a0878fd3b5b18ff3e2199b59aab5ef3
tbqq <tbqq@protonmail.com>
2022-11-08 10:55:59 +0100

    remove duplicated key

--- logger/config.go
+++ logger/config.go
@@ -4,8 +4,6 @@ type Config struct {
 	JSON   bool   `yaml:"json"`
 	Level  string `yaml:"level"`
 	Sample bool   `yaml:"sample,omitempty"`
-	// when sampling, the first N logs will be logged
-	SampleInitial int `yaml:"sampleInitial,omitempty"`
-	// when sampling, every Mth log will be logged
+	// when sampling, every Nth log will be logged
 	SampleInterval int `yaml:"sampleInitial,omitempty"`
 }
