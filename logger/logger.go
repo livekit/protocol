46717d50706ea31340e457787472502accd1f8fa
David Zhao <dz@livekit.io>
2022-12-08 23:53:20 -0800

    Support for participant-level log sampling (#257)

--- logger/logger.go
+++ logger/logger.go
@@ -1,65 +1,37 @@
 package logger
 
 import (
+	"time"
+
 	"github.com/go-logr/logr"
-	"github.com/go-logr/zapr"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var (
-	defaultLogger = logr.Discard()
-	pkgLogger     = Logger(logr.Discard())
+	discardLogger        = logr.Discard()
+	defaultLogger Logger = LogRLogger(discardLogger)
+	pkgLogger     Logger = LogRLogger(discardLogger)
 )
 
 // InitFromConfig initializes a Zap-based logger
 func InitFromConfig(conf Config, name string) {
-	lvl := ParseZapLevel(conf.Level)
-	zapConfig := zap.Config{
-		Level:            zap.NewAtomicLevelAt(lvl),
-		Development:      false,
-		Encoding:         "console",
-		EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
-		OutputPaths:      []string{"stderr"},
-		ErrorOutputPaths: []string{"stderr"},
+	l, err := NewZapLogger(&conf)
+	if err == nil {
+		SetLogger(l, name)
 	}
-	if conf.Sample {
-		zapConfig.Sampling = &zap.SamplingConfig{
-			Initial:    conf.SampleInitial,
-			Thereafter: conf.SampleInterval,
-		}
-		// sane defaults
-		if zapConfig.Sampling.Initial == 0 {
-			zapConfig.Sampling.Initial = 10
-		}
-		if zapConfig.Sampling.Thereafter == 0 {
-			zapConfig.Sampling.Thereafter = 100
-		}
-	}
-	if conf.JSON {
-		zapConfig.Encoding = "json"
-		zapConfig.EncoderConfig = zap.NewProductionEncoderConfig()
-	}
-	l, _ := zapConfig.Build()
-	zapLogger := zapr.NewLogger(l)
-	SetLogger(zapLogger, name)
 }
 
 // GetLogger returns the logger that was set with SetLogger with an extra depth of 1
-func GetLogger() logr.Logger {
+func GetLogger() Logger {
 	return defaultLogger
 }
 
-// GetDefaultLogger returns the logger that was set but with LiveKit wrappers
-func GetDefaultLogger() Logger {
-	return Logger(defaultLogger)
-}
-
 // SetLogger lets you use a custom logger. Pass in a logr.Logger with default depth
-func SetLogger(l logr.Logger, name string) {
+func SetLogger(l Logger, name string) {
 	defaultLogger = l.WithCallDepth(1).WithName(name)
 	// pkg wrapper needs to drop two levels of depth
-	pkgLogger = Logger(l.WithCallDepth(2).WithName(name))
+	pkgLogger = l.WithCallDepth(2).WithName(name)
 }
 
 func Debugw(msg string, keysAndValues ...interface{}) {
@@ -86,30 +58,164 @@ func ParseZapLevel(level string) zapcore.Level {
 	return lvl
 }
 
-type Logger logr.Logger
+type Logger interface {
+	Debugw(msg string, keysAndValues ...interface{})
+	Infow(msg string, keysAndValues ...interface{})
+	Warnw(msg string, err error, keysAndValues ...interface{})
+	Errorw(msg string, err error, keysAndValues ...interface{})
+	WithValues(keysAndValues ...interface{}) Logger
+	WithName(name string) Logger
+	WithCallDepth(depth int) Logger
+	WithItemSampler() Logger
+}
 
-func (l Logger) toLogr() logr.Logger {
+type ZapLogger struct {
+	zap *zap.SugaredLogger
+	// avoid multiple item samplers
+	hasItemSampler bool
+	SampleDuration time.Duration
+	SampleInitial  int
+	SampleInterval int
+}
+
+func NewZapLogger(conf *Config) (*ZapLogger, error) {
+	lvl := ParseZapLevel(conf.Level)
+	zapConfig := zap.Config{
+		Level:            zap.NewAtomicLevelAt(lvl),
+		Development:      false,
+		Encoding:         "console",
+		EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	if conf.Sample {
+		zapConfig.Sampling = &zap.SamplingConfig{
+			Initial:    conf.SampleInitial,
+			Thereafter: conf.SampleInterval,
+		}
+		// sane defaults
+		if zapConfig.Sampling.Initial == 0 {
+			zapConfig.Sampling.Initial = 10
+		}
+		if zapConfig.Sampling.Thereafter == 0 {
+			zapConfig.Sampling.Thereafter = 100
+		}
+	}
+	if conf.JSON {
+		zapConfig.Encoding = "json"
+		zapConfig.EncoderConfig = zap.NewProductionEncoderConfig()
+	}
+	l, err := zapConfig.Build()
+	if err != nil {
+		return nil, err
+	}
+	zl := &ZapLogger{
+		zap:            l.Sugar(),
+		SampleDuration: time.Duration(conf.ItemSampleSeconds) * time.Second,
+		SampleInitial:  conf.ItemSampleInitial,
+		SampleInterval: conf.ItemSampleInterval,
+	}
+	return zl, nil
+}
+
+func (l *ZapLogger) Debugw(msg string, keysAndValues ...interface{}) {
+	l.zap.Debugw(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) Infow(msg string, keysAndValues ...interface{}) {
+	l.zap.Infow(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) Warnw(msg string, err error, keysAndValues ...interface{}) {
+	if err != nil {
+		keysAndValues = append(keysAndValues, "error", err)
+	}
+	l.zap.Warnw(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) Errorw(msg string, err error, keysAndValues ...interface{}) {
+	if err != nil {
+		keysAndValues = append(keysAndValues, "error", err)
+	}
+	l.zap.Errorw(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) WithValues(keysAndValues ...interface{}) Logger {
+	dup := *l
+	dup.zap = l.zap.With(keysAndValues...)
+	return &dup
+}
+
+func (l *ZapLogger) WithName(name string) Logger {
+	dup := *l
+	dup.zap = l.zap.Named(name)
+	return &dup
+}
+
+func (l *ZapLogger) WithCallDepth(depth int) Logger {
+	dup := *l
+	dup.zap = l.zap.WithOptions(zap.AddCallerSkip(depth))
+	return &dup
+}
+
+func (l *ZapLogger) WithItemSampler() Logger {
+	if l.hasItemSampler || l.SampleDuration == 0 {
+		return l
+	}
+	dup := *l
+	dup.zap = l.zap.WithOptions(zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+		return zapcore.NewSamplerWithOptions(
+			core,
+			l.SampleDuration,
+			l.SampleInitial,
+			l.SampleInterval,
+		)
+	}))
+	dup.hasItemSampler = true
+	return &dup
+}
+
+type LogRLogger logr.Logger
+
+func (l LogRLogger) toLogr() logr.Logger {
 	if logr.Logger(l).GetSink() == nil {
-		return defaultLogger
+		return discardLogger
 	}
 	return logr.Logger(l)
 }
 
-func (l Logger) Debugw(msg string, keysAndValues ...interface{}) {
+func (l LogRLogger) Debugw(msg string, keysAndValues ...interface{}) {
 	l.toLogr().V(1).Info(msg, keysAndValues...)
 }
 
-func (l Logger) Infow(msg string, keysAndValues ...interface{}) {
+func (l LogRLogger) Infow(msg string, keysAndValues ...interface{}) {
 	l.toLogr().Info(msg, keysAndValues...)
 }
 
-func (l Logger) Warnw(msg string, err error, keysAndValues ...interface{}) {
+func (l LogRLogger) Warnw(msg string, err error, keysAndValues ...interface{}) {
 	if err != nil {
 		keysAndValues = append(keysAndValues, "error", err)
 	}
 	l.toLogr().Info(msg, keysAndValues...)
 }
 
-func (l Logger) Errorw(msg string, err error, keysAndValues ...interface{}) {
+func (l LogRLogger) Errorw(msg string, err error, keysAndValues ...interface{}) {
 	l.toLogr().Error(err, msg, keysAndValues...)
+}
+
+func (l LogRLogger) WithValues(keysAndValues ...interface{}) Logger {
+	return LogRLogger(l.toLogr().WithValues(keysAndValues...))
+}
+
+func (l LogRLogger) WithName(name string) Logger {
+	return LogRLogger(l.toLogr().WithName(name))
+}
+
+func (l LogRLogger) WithCallDepth(depth int) Logger {
+	return LogRLogger(l.toLogr().WithCallDepth(depth))
+}
+
+func (l LogRLogger) WithItemSampler() Logger {
+	// logr does not support sampling
+	return l
 }
