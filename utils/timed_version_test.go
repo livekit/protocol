fb9cde61491768ed59f0995b5c11321e7a2a6bf5
paulwe <paulwe@gmail.com>
2023-02-18 11:31:25 -0800

    assert -> require

--- utils/timed_version_test.go
+++ utils/timed_version_test.go
@@ -3,7 +3,7 @@ package utils
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTimedVersion(t *testing.T) {
@@ -13,24 +13,24 @@ func TestTimedVersion(t *testing.T) {
 		tv2 := gen.New()
 		tv3 := gen.New()
 
-		assert.True(t, tv3.After(tv1))
-		assert.True(t, tv3.After(tv2))
-		assert.True(t, tv2.After(tv1))
+		require.True(t, tv3.After(tv1))
+		require.True(t, tv3.After(tv2))
+		require.True(t, tv2.After(tv1))
 
 		tv2.Update(tv3)
-		assert.True(t, tv2.After(tv1))
+		require.True(t, tv2.After(tv1))
 		// tv3 and tv2 are equivalent after update
-		assert.False(t, tv3.After(tv2))
+		require.False(t, tv3.After(tv2))
 
-		assert.Equal(t, 0, tv1.Compare(tv1))
-		assert.Equal(t, -1, tv1.Compare(tv2))
-		assert.Equal(t, 1, tv2.Compare(tv1))
+		require.Equal(t, 0, tv1.Compare(tv1))
+		require.Equal(t, -1, tv1.Compare(tv2))
+		require.Equal(t, 1, tv2.Compare(tv1))
 	})
 
 	t.Run("protobuf roundtrip", func(t *testing.T) {
 		gen := NewDefaultTimedVersionGenerator()
 		tv1 := gen.New()
 		tv2 := NewTimedVersionFromProto(tv1.ToProto())
-		assert.Equal(t, tv1.v.Load(), tv2.v.Load())
+		require.Equal(t, tv1.v.Load(), tv2.v.Load())
 	})
 }
