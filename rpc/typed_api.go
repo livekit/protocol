c0705dbb696a47cf28ca946a7909fd6990decaae
Paul Wells <paulwe@gmail.com>
2023-03-02 18:56:09 -0800

    Add signal relay interface (#313)
    * add signal psrpc interface
    
    * generated protobuf
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ rpc/typed_api.go
@@ -0,0 +1,17 @@
+package rpc
+
+import (
+	"github.com/livekit/protocol/livekit"
+	"github.com/livekit/psrpc"
+)
+
+type TypedSignalClient = SignalClient[livekit.NodeID]
+type TypedSignalServer = SignalServer[livekit.NodeID]
+
+func NewTypedSignalClient(nodeID livekit.NodeID, bus psrpc.MessageBus, opts ...psrpc.ClientOption) (TypedSignalClient, error) {
+	return NewSignalClient[livekit.NodeID](string(nodeID), bus, opts...)
+}
+
+func NewTypedSignalServer(nodeID livekit.NodeID, svc SignalServerImpl, bus psrpc.MessageBus, opts ...psrpc.ServerOption) (TypedSignalServer, error) {
+	return NewSignalServer[livekit.NodeID](string(nodeID), svc, bus, opts...)
+}
