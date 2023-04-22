4eebb41d5e280dfb410dbf395b26edd910fba3ea
boks1971 <raja.gobi@tutanota.com>
2023-04-22 09:26:13 +0530

    Moving timeseries to own module and adding more stats/tests.

---
+++ utils/timeseries/timeseries.go
@@ -0,0 +1,444 @@
+package timeseries
+
+import (
+	"container/list"
+	"fmt"
+	"math"
+	"sync"
+	"time"
+)
+
+const (
+	minLinearFitSamples = 5
+)
+
+// ------------------------------------------------
+
+type TimeSeriesUpdateOp int
+
+const (
+	TimeSeriesUpdateOpAdd TimeSeriesUpdateOp = iota
+	TimeSeriesUpdateOpMax
+)
+
+func (t TimeSeriesUpdateOp) String() string {
+	switch t {
+	case TimeSeriesUpdateOpAdd:
+		return "ADD"
+	case TimeSeriesUpdateOpMax:
+		return "MAX"
+	default:
+		return fmt.Sprintf("%d", int(t))
+	}
+}
+
+// ------------------------------------------------
+
+type TimeSeriesCompareOp int
+
+const (
+	TimeSeriesCompareOpEQ TimeSeriesCompareOp = iota
+	TimeSeriesCompareOpNE
+	TimeSeriesCompareOpGT
+	TimeSeriesCompareOpGTE
+	TimeSeriesCompareOpLT
+	TimeSeriesCompareOpLTE
+)
+
+func (t TimeSeriesCompareOp) String() string {
+	switch t {
+	case TimeSeriesCompareOpEQ:
+		return "EQ"
+	case TimeSeriesCompareOpNE:
+		return "NE"
+	case TimeSeriesCompareOpGT:
+		return "GT"
+	case TimeSeriesCompareOpGTE:
+		return "GTE"
+	case TimeSeriesCompareOpLT:
+		return "LT"
+	case TimeSeriesCompareOpLTE:
+		return "LTE"
+	default:
+		return fmt.Sprintf("%d", int(t))
+	}
+}
+
+// ------------------------------------------------
+
+type number interface {
+	uint32 | float64
+}
+
+type TimeSeriesSample[T number] struct {
+	Value T
+	At    time.Time
+}
+
+type TimeSeriesParams struct {
+	UpdateOp TimeSeriesUpdateOp
+	Window   time.Duration
+}
+
+type TimeSeries[T number] struct {
+	params TimeSeriesParams
+
+	lock           sync.RWMutex
+	samples        *list.List
+	activeSample   T
+	isActiveSample bool
+
+	welfordCount int
+	welfordM     float64
+	welfordS     float64
+	welfordStart time.Time
+	welfordLast  time.Time
+}
+
+func NewTimeSeries[T number](params TimeSeriesParams) *TimeSeries[T] {
+	t := &TimeSeries[T]{
+		params:  params,
+		samples: list.New(),
+	}
+
+	t.initSamples()
+	return t
+}
+
+func (t *TimeSeries[T]) UpdateSample(val T) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	if !t.isActiveSample {
+		t.isActiveSample = true
+		t.activeSample = val
+		return
+	}
+
+	switch t.params.UpdateOp {
+	case TimeSeriesUpdateOpAdd:
+		t.activeSample += val
+	case TimeSeriesUpdateOpMax:
+		if val > t.activeSample {
+			t.activeSample = val
+		}
+	}
+}
+
+func (t *TimeSeries[T]) CommitActiveSample() {
+	t.CommitActiveSampleAt(time.Now())
+}
+
+func (t *TimeSeries[T]) CommitActiveSampleAt(at time.Time) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	if !t.isActiveSample {
+		return
+	}
+
+	t.addSampleAt(t.activeSample, at)
+	t.isActiveSample = false
+}
+
+func (t *TimeSeries[T]) AddSample(val T) {
+	t.AddSampleAt(val, time.Now())
+}
+
+func (t *TimeSeries[T]) AddSampleAt(val T, at time.Time) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.addSampleAt(val, at)
+}
+
+func (t *TimeSeries[T]) GetSamples() []TimeSeriesSample[T] {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	samples := make([]TimeSeriesSample[T], 0, t.samples.Len())
+	for e := t.samples.Front(); e != nil; e = e.Next() {
+		samples = append(samples, e.Value.(TimeSeriesSample[T]))
+	}
+	return samples
+}
+
+func (t *TimeSeries[T]) ClearSamples() {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.initSamples()
+}
+
+func (t *TimeSeries[T]) Sum() float64 {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.prune()
+
+	sum := float64(0.0)
+	for e := t.samples.Front(); e != nil; e = e.Next() {
+		s := e.Value.(TimeSeriesSample[T])
+		sum += float64(s.Value)
+	}
+
+	return sum
+}
+
+func (t *TimeSeries[T]) Min() T {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.prune()
+	return t.minLocked()
+}
+
+func (t *TimeSeries[T]) minLocked() T {
+	min := T(0)
+	for e := t.samples.Front(); e != nil; e = e.Next() {
+		s := e.Value.(TimeSeriesSample[T])
+		if min == T(0) || min > s.Value {
+			min = s.Value
+		}
+	}
+
+	return min
+}
+
+func (t *TimeSeries[T]) Max() T {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.prune()
+
+	return t.maxLocked()
+}
+
+func (t *TimeSeries[T]) maxLocked() T {
+	max := T(0)
+	for e := t.samples.Front(); e != nil; e = e.Next() {
+		s := e.Value.(TimeSeriesSample[T])
+		if max < s.Value {
+			max = s.Value
+		}
+	}
+
+	return max
+}
+
+func (t *TimeSeries[T]) CurrentRun(threshold T, op TimeSeriesCompareOp) time.Duration {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.prune()
+
+	start := time.Time{}
+	end := time.Time{}
+
+	for e := t.samples.Back(); e != nil; e = e.Prev() {
+		cond := false
+		s := e.Value.(TimeSeriesSample[T])
+		switch op {
+		case TimeSeriesCompareOpEQ:
+			cond = s.Value == threshold
+		case TimeSeriesCompareOpNE:
+			cond = s.Value != threshold
+		case TimeSeriesCompareOpGT:
+			cond = s.Value > threshold
+		case TimeSeriesCompareOpGTE:
+			cond = s.Value >= threshold
+		case TimeSeriesCompareOpLT:
+			cond = s.Value < threshold
+		case TimeSeriesCompareOpLTE:
+			cond = s.Value <= threshold
+		}
+		if !cond {
+			break
+		}
+		if end.IsZero() {
+			end = s.At
+		}
+		start = s.At
+	}
+
+	if end.IsZero() || start.IsZero() {
+		return 0
+	}
+
+	return end.Sub(start)
+}
+
+func (t *TimeSeries[T]) OnlineAverage() float64 {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	return t.welfordM
+}
+
+func (t *TimeSeries[T]) OnlineVariance() float64 {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	return t.onlineVarianceLocked()
+}
+
+func (t *TimeSeries[T]) onlineVarianceLocked() float64 {
+	if t.welfordCount > 1 {
+		return t.welfordS / float64(t.welfordCount-1)
+	}
+
+	return 0.0
+}
+
+func (t *TimeSeries[T]) OnlineStdDev() float64 {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	return t.onlineStdDevLocked()
+}
+
+func (t *TimeSeries[T]) onlineStdDevLocked() float64 {
+	return math.Sqrt(t.onlineVarianceLocked())
+}
+
+func (t *TimeSeries[T]) ZScore(val T) float64 {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	onlineStdDev := t.onlineStdDevLocked()
+	if onlineStdDev != 0.0 {
+		return (float64(val) - t.welfordM) / t.onlineStdDevLocked()
+	}
+
+	return 0.0
+}
+
+func (t *TimeSeries[T]) LinearFit() (float64, float64) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.prune()
+
+	if t.samples.Len() < minLinearFitSamples {
+		return 0.0, 0.0
+	}
+
+	min := t.minLocked()
+	max := t.maxLocked()
+	diff := float64(max - min)
+
+	start := time.Time{}
+
+	sx := float64(0.0)
+	sxsq := float64(0.0)
+	sy := float64(0.0)
+	sysq := float64(0.0)
+	sxy := float64(0.0)
+
+	for e := t.samples.Front(); e != nil; e = e.Next() {
+		s := e.Value.(TimeSeriesSample[T])
+		if start.IsZero() {
+			start = s.At
+		}
+		since := s.At.Sub(start).Seconds()
+		normy := 0.0
+		if diff != 0.0 {
+			normy = float64(s.Value-min) / diff
+		}
+		sx += since
+		sxsq += since * since
+		sy += normy
+		sysq += normy * normy
+		sxy += since * normy
+	}
+
+	N := float64(t.samples.Len())
+	sxwsq := sx * sx
+	denom := N*sxsq - sxwsq
+	slope := float64(0.0)
+	if denom != 0.0 {
+		slope = (N*sxy - sx*sy) / denom
+	}
+	fmt.Printf("N: %f, sxy: %f, sx: %f, sy: %f, denom: %f, slope: %f\n", N, sxy, sx, sy, denom, slope) // REMOVE
+	intercept := (sy - slope*sx) / N
+	return slope, intercept
+}
+
+func (t *TimeSeries[T]) initSamples() {
+	t.samples = t.samples.Init()
+}
+
+func (t *TimeSeries[T]) addSampleAt(val T, at time.Time) {
+	// insert in time order
+	var e *list.Element
+	for e = t.samples.Back(); e != nil; e = e.Prev() {
+		s := e.Value.(TimeSeriesSample[T])
+		if at.After(s.At) {
+			break
+		}
+	}
+
+	sample := TimeSeriesSample[T]{
+		Value: val,
+		At:    at,
+	}
+	switch {
+	case e != nil: // in the middle
+		t.samples.InsertAfter(sample, e)
+
+	case t.samples.Front() != nil: // in the front
+		t.samples.PushFront(sample)
+
+	default: // at the end
+		t.samples.PushBack(sample)
+	}
+
+	t.updateWelfordStats(val, at)
+
+	t.prune()
+}
+
+func (t *TimeSeries[T]) updateWelfordStats(val T, at time.Time) {
+	t.welfordCount++
+	mLast := t.welfordM
+	t.welfordM += (float64(val) - t.welfordM) / float64(t.welfordCount)
+	t.welfordS += (float64(val) - mLast) * (float64(val) - t.welfordM)
+
+	if t.welfordStart.IsZero() {
+		t.welfordStart = at
+	}
+	t.welfordLast = at
+}
+
+func (t *TimeSeries[T]) prune() {
+	thresh := t.welfordLast.Add(-t.params.Window)
+	//thresh := time.Now().Add(-t.params.Window)
+
+	toRemove := make([]*list.Element, 0, t.samples.Len())
+	for e := t.samples.Front(); e != nil; e = e.Next() {
+		s := e.Value.(TimeSeriesSample[T])
+		if s.At.After(thresh) {
+			break
+		}
+
+		toRemove = append(toRemove, e)
+	}
+
+	for _, e := range toRemove {
+		t.samples.Remove(e)
+	}
+}
+
+// TODO - a bunch of stats
+// - sum
+// - moving average
+// - EWMA
+// - min
+// - max
+// - average
+// - median
+// - variance
+// - stddev
+// - trend
+// - run
+// - z-score
---
+++ utils/timeseries/timeseries_test.go
@@ -0,0 +1,333 @@
+package timeseries
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTimeSeries(t *testing.T) {
+	t.Run("ordering", func(t *testing.T) {
+		ts := NewTimeSeries[uint32](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpMax,
+			Window:   time.Minute,
+		})
+
+		now := time.Now()
+		expectedSamples := []TimeSeriesSample[uint32]{
+			{
+				Value: 10,
+				At:    now,
+			},
+			{
+				Value: 20,
+				At:    now.Add(time.Second),
+			},
+			{
+				Value: 30,
+				At:    now.Add(2 * time.Second),
+			},
+			{
+				Value: 40,
+				At:    now.Add(3 * time.Second),
+			},
+		}
+
+		testCases := []struct {
+			name    string
+			samples []TimeSeriesSample[uint32]
+		}{
+			{
+				name: "regular",
+				samples: []TimeSeriesSample[uint32]{
+					{10, now},
+					{20, now.Add(time.Second)},
+					{30, now.Add(2 * time.Second)},
+					{40, now.Add(3 * time.Second)},
+				},
+			},
+			{
+				name: "reverse",
+				samples: []TimeSeriesSample[uint32]{
+					{40, now.Add(3 * time.Second)},
+					{30, now.Add(2 * time.Second)},
+					{20, now.Add(time.Second)},
+					{10, now},
+				},
+			},
+			{
+				name: "jumbled 1",
+				samples: []TimeSeriesSample[uint32]{
+					{20, now.Add(time.Second)},
+					{40, now.Add(3 * time.Second)},
+					{30, now.Add(2 * time.Second)},
+					{10, now},
+				},
+			},
+			{
+				name: "jumbled 2",
+				samples: []TimeSeriesSample[uint32]{
+					{10, now},
+					{40, now.Add(3 * time.Second)},
+					{30, now.Add(2 * time.Second)},
+					{20, now.Add(time.Second)},
+				},
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				ts.ClearSamples()
+
+				for _, tss := range tc.samples {
+					ts.AddSampleAt(tss.Value, tss.At)
+				}
+
+				require.Equal(t, expectedSamples, ts.GetSamples())
+			})
+		}
+	})
+
+	t.Run("sum", func(t *testing.T) {
+		ts := NewTimeSeries[uint32](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpMax,
+			Window:   2 * time.Minute,
+		})
+
+		ts.UpdateSample(10)
+		ts.UpdateSample(20)
+		ts.CommitActiveSampleAt(time.Now())
+		require.Equal(t, float64(20.0), ts.Sum())
+
+		ts.AddSample(30)
+		require.Equal(t, float64(50.0), ts.Sum())
+	})
+
+	t.Run("min", func(t *testing.T) {
+		ts := NewTimeSeries[uint32](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpMax,
+			Window:   2 * time.Minute,
+		})
+
+		ts.UpdateSample(10)
+		ts.UpdateSample(20)
+		ts.CommitActiveSampleAt(time.Now())
+		require.Equal(t, uint32(20), ts.Min())
+
+		ts.AddSample(30)
+		require.Equal(t, uint32(20), ts.Min())
+	})
+
+	t.Run("max", func(t *testing.T) {
+		ts := NewTimeSeries[uint32](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpAdd,
+			Window:   2 * time.Minute,
+		})
+
+		ts.UpdateSample(10)
+		ts.UpdateSample(20)
+		ts.CommitActiveSampleAt(time.Now())
+		require.Equal(t, uint32(30), ts.Max())
+
+		ts.AddSample(20)
+		require.Equal(t, uint32(30), ts.Max())
+	})
+
+	t.Run("current_run", func(t *testing.T) {
+		ts := NewTimeSeries[uint32](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpMax,
+			Window:   time.Minute,
+		})
+
+		testCases := []struct {
+			name           string
+			values         []uint32
+			timeStep       time.Duration
+			compareOp      TimeSeriesCompareOp
+			threshold      uint32
+			expectedResult time.Duration
+		}{
+			{
+				name: "eq_run",
+				values: []uint32{
+					10,
+					20,
+					30,
+					40,
+					40,
+					40,
+				},
+				timeStep:       time.Second,
+				compareOp:      TimeSeriesCompareOpEQ,
+				threshold:      40,
+				expectedResult: 2 * time.Second,
+			},
+			{
+				name: "eq_no_run",
+				values: []uint32{
+					10,
+					20,
+					30,
+					40,
+					40,
+					40,
+				},
+				timeStep:       time.Second,
+				compareOp:      TimeSeriesCompareOpEQ,
+				threshold:      50,
+				expectedResult: 0,
+			},
+			{
+				name: "ne",
+				values: []uint32{
+					10,
+					20,
+					30,
+					40,
+					40,
+					40,
+				},
+				timeStep:       time.Second,
+				compareOp:      TimeSeriesCompareOpNE,
+				threshold:      50,
+				expectedResult: 5 * time.Second,
+			},
+			{
+				name: "gt",
+				values: []uint32{
+					10,
+					20,
+					30,
+					40,
+					40,
+					40,
+				},
+				timeStep:       time.Second,
+				compareOp:      TimeSeriesCompareOpGT,
+				threshold:      20,
+				expectedResult: 3 * time.Second,
+			},
+			{
+				name: "gte",
+				values: []uint32{
+					10,
+					20,
+					30,
+					40,
+					40,
+					40,
+				},
+				timeStep:       time.Second,
+				compareOp:      TimeSeriesCompareOpGTE,
+				threshold:      20,
+				expectedResult: 4 * time.Second,
+			},
+			{
+				name: "lt",
+				values: []uint32{
+					50,
+					20,
+					30,
+					40,
+					40,
+					40,
+				},
+				timeStep:       time.Second,
+				compareOp:      TimeSeriesCompareOpLT,
+				threshold:      50,
+				expectedResult: 4 * time.Second,
+			},
+			{
+				name: "lte",
+				values: []uint32{
+					10,
+					20,
+					30,
+					40,
+					40,
+					40,
+				},
+				timeStep:       time.Second,
+				compareOp:      TimeSeriesCompareOpLTE,
+				threshold:      40,
+				expectedResult: 5 * time.Second,
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				ts.ClearSamples()
+
+				now := time.Now()
+				for idx, value := range tc.values {
+					ts.AddSampleAt(value, now.Add(time.Duration(idx)*tc.timeStep))
+				}
+
+				require.Equal(t, tc.expectedResult, ts.CurrentRun(tc.threshold, tc.compareOp))
+			})
+		}
+	})
+
+	t.Run("online", func(t *testing.T) {
+		ts := NewTimeSeries[uint32](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpMax,
+			Window:   time.Minute,
+		})
+
+		now := time.Now()
+		for val := uint32(1); val <= 10; val++ {
+			ts.AddSampleAt(val, now.Add(time.Duration(val)*time.Second))
+		}
+
+		require.Equal(t, float64(5.5), ts.OnlineAverage())
+		onlineVariance := ts.OnlineVariance()
+		require.Condition(t, func() bool { return onlineVariance > 9.16 && onlineVariance < 9.17 }, "online variance out of range")
+		onlineStdDev := ts.OnlineStdDev()
+		require.Condition(t, func() bool { return onlineStdDev > 3.02 && onlineStdDev < 3.03 }, "online std dev out of range")
+	})
+
+	/* RAJA-REMOVE
+	t.Run("z-score", func(t *testing.T) {
+		ts := NewTimeSeries[float64](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpMax,
+			Window:   time.Minute,
+		})
+
+		now := time.Now()
+		for idx := uint32(1); idx <= 10; idx++ {
+			ts.AddSampleAt(0.1, now.Add(time.Duration(idx)*time.Second))
+			fmt.Printf("m: %f, s: %f, z: %f\n", ts.OnlineAverage(), ts.OnlineStdDev(), ts.ZScore(0.1)) // REMOVE
+		}
+		for idx := uint32(11); idx <= 20; idx++ {
+			ts.AddSampleAt(0.5, now.Add(time.Duration(idx)*time.Second))
+			fmt.Printf("m: %f, s: %f, z: %f\n", ts.OnlineAverage(), ts.OnlineStdDev(), ts.ZScore(0.5)) // REMOVE
+		}
+	})
+	*/
+
+	t.Run("linear fit", func(t *testing.T) {
+		ts := NewTimeSeries[float64](TimeSeriesParams{
+			UpdateOp: TimeSeriesUpdateOpMax,
+			Window:   20 * time.Second,
+		})
+
+		now := time.Now()
+		for idx := uint32(1); idx <= 10; idx++ {
+			ts.AddSampleAt(0.1, now.Add(time.Duration(idx)*time.Second))
+			slope, intercept := ts.LinearFit()
+			fmt.Printf("m: %f, s: %f, z: %f, s: %f, i: %f\n", ts.OnlineAverage(), ts.OnlineStdDev(), ts.ZScore(0.1), slope, intercept) // REMOVE
+		}
+		for idx := uint32(11); idx <= 20; idx++ {
+			ts.AddSampleAt(0.5, now.Add(time.Duration(idx)*time.Second))
+			slope, intercept := ts.LinearFit()
+			fmt.Printf("m: %f, s: %f, z: %f, s: %f, i: %f\n", ts.OnlineAverage(), ts.OnlineStdDev(), ts.ZScore(0.5), slope, intercept) // REMOVE
+		}
+		for idx := uint32(21); idx <= 30; idx++ {
+			ts.AddSampleAt(0.1, now.Add(time.Duration(idx)*time.Second))
+			slope, intercept := ts.LinearFit()
+			fmt.Printf("m: %f, s: %f, z: %f, s: %f, i: %f\n", ts.OnlineAverage(), ts.OnlineStdDev(), ts.ZScore(0.1), slope, intercept) // REMOVE
+		}
+	})
+}
