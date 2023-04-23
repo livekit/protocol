a68f38ce7d7a186410f5e40d835d10048fc55117
David Zhao <dz@livekit.io>
2023-04-22 20:25:51 -0700

    Added ProtoProxy util, NumPublishers field (#358)
    ProtoProxy achieves two goals:
    - avoids needless regeneration of readonly protobufs
    - update certain changes periodically

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,11 +31,13 @@ function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4
 
 if [ $(version $GO_VERSION) -ge $(version $GO_TARGET_VERSION) ];
   then
-    go install github.com/twitchtv/twirp/protoc-gen-twirp@v8.1.0
-    go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
+    go install github.com/twitchtv/twirp/protoc-gen-twirp@v8.1.3
+    go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.30.0
     go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+    go install github.com/livekit/psrpc/protoc-gen-psrpc@v0.3.0
   else
-    go get -u github.com/twitchtv/twirp/protoc-gen-twirp@v8.1.0
-    go get -u google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
+    go get -u github.com/twitchtv/twirp/protoc-gen-twirp@v8.1.3
+    go get -u google.golang.org/protobuf/cmd/protoc-gen-go@v1.30.0
     go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+    go get -u github.com/livekit/psrpc/protoc-gen-psrpc@v0.3.0
 fi
---
+++ utils/protoproxy_test.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"runtime"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/livekit/protocol/livekit"
+)
+
+func TestProtoProxy(t *testing.T) {
+	numGoRoutines := runtime.NumGoroutine()
+	proxy, numParticipants := createTestProxy()
+
+	select {
+	case <-proxy.Updated():
+		t.Fatal("should not have received an update")
+	default:
+	}
+
+	// should not have changed, initial value should persist
+	require.EqualValues(t, 0, proxy.Get().NumParticipants)
+	require.EqualValues(t, 0, proxy.Get().NumParticipants)
+
+	// immediate change
+	proxy.MarkDirty(true)
+	time.Sleep(100 * time.Millisecond)
+
+	require.EqualValues(t, 2, numParticipants.Load())
+	require.EqualValues(t, 1, proxy.Get().NumParticipants)
+
+	// queued updates
+	proxy.MarkDirty(false)
+	select {
+	case <-proxy.Updated():
+		// consume previous notification
+	default:
+	}
+	require.EqualValues(t, 1, proxy.Get().NumParticipants)
+
+	select {
+	case <-proxy.Updated():
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("should have received an update")
+	}
+	require.EqualValues(t, 2, proxy.Get().NumParticipants)
+
+	// ensure we didn't leak
+	proxy.Stop()
+	require.Equal(t, numGoRoutines, runtime.NumGoroutine())
+}
+
+func createTestProxy() (*ProtoProxy[*livekit.Room], *atomic.Uint32) {
+	// uses an update func that increments numParticipants each time
+	var numParticipants atomic.Uint32
+	return NewProtoProxy[*livekit.Room](10*time.Millisecond, func() *livekit.Room {
+		defer numParticipants.Add(1)
+		return &livekit.Room{
+			NumParticipants: numParticipants.Load(),
+		}
+	}), &numParticipants
+}
