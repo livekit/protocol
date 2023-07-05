424b21fd0e92a69c81c0debd1f3766ea35cc5a18
David Colburn <xero73@gmail.com>
2023-07-05 14:55:02 -0700

    retry on egress timeout or resource exhausted (#414)

--- rpc/egress_client.go
+++ rpc/egress_client.go
@@ -1,9 +1,14 @@
 package rpc
 
 import (
+	"errors"
+
 	"github.com/livekit/protocol/livekit"
 	"github.com/livekit/psrpc"
+	"github.com/livekit/psrpc/pkg/middleware"
 )
+
+const resourceExhaustedRetries = 3
 
 type EgressClient interface {
 	EgressInternalClient
@@ -21,14 +26,26 @@ func NewEgressClient(nodeID livekit.NodeID, bus psrpc.MessageBus) (EgressClient,
 	}
 
 	clientID := string(nodeID)
-	internalClient, err := NewEgressInternalClient(clientID, bus)
+	internalClient, err := NewEgressInternalClient(clientID, bus, middleware.WithRPCRetries(middleware.RetryOptions{
+		MaxAttempts: 3,
+		Timeout:     psrpc.DefaultClientTimeout,
+		IsRecoverable: func(err error) bool {
+			var e psrpc.Error
+			if !errors.As(err, &e) {
+				return true
+			}
+			return e.Code() == psrpc.DeadlineExceeded || e.Code() == psrpc.ResourceExhausted
+		},
+	}))
 	if err != nil {
 		return nil, err
 	}
+
 	handlerClient, err := NewEgressHandlerClient(clientID, bus)
 	if err != nil {
 		return nil, err
 	}
+
 	return &egressClient{
 		EgressInternalClient: internalClient,
 		EgressHandlerClient:  handlerClient,
