52325eeda3207baa40bd4325a51bd90647fc5381
David Colburn <xero73@gmail.com>
2023-07-26 14:05:59 -0700

    export GetOutputType

--- egress/types.go
+++ egress/types.go
@@ -20,16 +20,16 @@ const (
 func GetTypes(info *livekit.EgressInfo) (string, string) {
 	switch req := info.Request.(type) {
 	case *livekit.EgressInfo_RoomComposite:
-		return EgressTypeRoomComposite, getOutputTypeDeprecated(req.RoomComposite)
+		return EgressTypeRoomComposite, GetOutputType(req.RoomComposite)
 
 	case *livekit.EgressInfo_Web:
-		return EgressTypeWeb, getOutputTypeDeprecated(req.Web)
+		return EgressTypeWeb, GetOutputType(req.Web)
 
 	case *livekit.EgressInfo_Participant:
-		return EgressTypeParticipant, getOutputType(req.Participant)
+		return EgressTypeParticipant, GetOutputType(req.Participant)
 
 	case *livekit.EgressInfo_TrackComposite:
-		return EgressTypeTrackComposite, getOutputTypeDeprecated(req.TrackComposite)
+		return EgressTypeTrackComposite, GetOutputType(req.TrackComposite)
 
 	case *livekit.EgressInfo_Track:
 		switch req.Track.Output.(type) {
@@ -43,33 +43,7 @@ func GetTypes(info *livekit.EgressInfo) (string, string) {
 	return Unknown, Unknown
 }
 
-func getOutputTypeDeprecated(req interface {
-	GetFile() *livekit.EncodedFileOutput
-	GetStream() *livekit.StreamOutput
-	GetSegments() *livekit.SegmentedFileOutput
-	GetFileOutputs() []*livekit.EncodedFileOutput
-	GetStreamOutputs() []*livekit.StreamOutput
-	GetSegmentOutputs() []*livekit.SegmentedFileOutput
-}) string {
-	hasFile := len(req.GetFileOutputs()) > 0
-	hasStream := len(req.GetStreamOutputs()) > 0
-	hasSegments := len(req.GetSegmentOutputs()) > 0
-
-	switch {
-	case (hasFile && (hasStream || hasSegments)) || (hasStream && hasSegments):
-		return OutputTypeMultiple
-	case hasFile || req.GetFile() != nil:
-		return OutputTypeFile
-	case hasStream || req.GetStream() != nil:
-		return OutputTypeStream
-	case hasSegments || req.GetSegments() != nil:
-		return OutputTypeSegments
-	}
-
-	return Unknown
-}
-
-func getOutputType(req interface {
+func GetOutputType(req interface {
 	GetFileOutputs() []*livekit.EncodedFileOutput
 	GetStreamOutputs() []*livekit.StreamOutput
 	GetSegmentOutputs() []*livekit.SegmentedFileOutput
@@ -87,6 +61,25 @@ func getOutputType(req interface {
 		return OutputTypeStream
 	case hasSegments:
 		return OutputTypeSegments
+	}
+
+	if r, ok := req.(interface {
+		GetFile() *livekit.EncodedFileOutput
+		GetStream() *livekit.StreamOutput
+		GetSegments() *livekit.SegmentedFileOutput
+		GetFileOutputs() []*livekit.EncodedFileOutput
+		GetStreamOutputs() []*livekit.StreamOutput
+		GetSegmentOutputs() []*livekit.SegmentedFileOutput
+	}); ok {
+		if r.GetFile() != nil {
+			return OutputTypeFile
+		}
+		if r.GetStream() != nil {
+			return OutputTypeStream
+		}
+		if r.GetSegments() != nil {
+			return OutputTypeSegments
+		}
 	}
 
 	return Unknown
