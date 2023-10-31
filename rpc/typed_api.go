543d414af39efe32e946d52f4d8d1c4b0eadf96c
paulwe <paulwe@gmail.com>
2023-10-30 20:42:59 -0700

    split room service

--- rpc/typed_api.go
+++ rpc/typed_api.go
@@ -17,10 +17,65 @@ package rpc
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/livekit/protocol/livekit"
+	"github.com/livekit/protocol/logger"
+	protopsrpc "github.com/livekit/protocol/psrpc"
+	"github.com/livekit/protocol/utils"
 	"github.com/livekit/psrpc"
+	"github.com/livekit/psrpc/pkg/middleware"
 )
+
+type PSRPCConfig struct {
+	Enabled     bool          `yaml:"enabled,omitempty"`
+	MaxAttempts int           `yaml:"max_attempts,omitempty"`
+	Timeout     time.Duration `yaml:"timeout,omitempty"`
+	Backoff     time.Duration `yaml:"backoff,omitempty"`
+	BufferSize  int           `yaml:"buffer_size,omitempty"`
+}
+
+var DefaultPSRPCConfig = PSRPCConfig{
+	MaxAttempts: 3,
+	Timeout:     500 * time.Millisecond,
+	Backoff:     500 * time.Millisecond,
+	BufferSize:  1000,
+}
+
+type ClientParams struct {
+	PSRPCConfig
+	Bus              psrpc.MessageBus
+	Logger           logger.Logger
+	Observer         middleware.MetricsObserver
+	VersionGenerator utils.TimedVersionGenerator
+}
+
+func NewClientParams(
+	config PSRPCConfig,
+	bus psrpc.MessageBus,
+	logger logger.Logger,
+	observer middleware.MetricsObserver,
+) ClientParams {
+	return ClientParams{
+		PSRPCConfig: config,
+		Bus:         bus,
+		Logger:      logger,
+		Observer:    observer,
+	}
+}
+
+func clientOptions(params ClientParams) []psrpc.ClientOption {
+	return []psrpc.ClientOption{
+		psrpc.WithClientChannelSize(params.BufferSize),
+		middleware.WithClientMetrics(params.Observer),
+		protopsrpc.WithClientLogger(params.Logger),
+		middleware.WithRPCRetries(middleware.RetryOptions{
+			MaxAttempts: params.MaxAttempts,
+			Timeout:     params.Timeout,
+			Backoff:     params.Backoff,
+		}),
+	}
+}
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 
@@ -46,19 +101,45 @@ func FormatRoomTopic(roomName livekit.RoomName) RoomTopic {
 	return RoomTopic(roomName)
 }
 
+type topicFormatter struct{}
+
+func NewTopicFormatter() TopicFormatter {
+	return topicFormatter{}
+}
+
+func (f topicFormatter) ParticipantTopic(ctx context.Context, roomName livekit.RoomName, identity livekit.ParticipantIdentity) ParticipantTopic {
+	return FormatParticipantTopic(roomName, identity)
+}
+
+func (f topicFormatter) RoomTopic(ctx context.Context, roomName livekit.RoomName) RoomTopic {
+	return FormatRoomTopic(roomName)
+}
+
 type TopicFormatter interface {
 	ParticipantTopic(ctx context.Context, roomName livekit.RoomName, identity livekit.ParticipantIdentity) ParticipantTopic
 	RoomTopic(ctx context.Context, roomName livekit.RoomName) RoomTopic
 }
 
 //counterfeiter:generate . TypedRoomClient
-type TypedRoomClient = RoomClient[ParticipantTopic, RoomTopic]
-type TypedRoomServer = RoomServer[ParticipantTopic, RoomTopic]
+type TypedRoomClient = RoomClient[RoomTopic]
+type TypedRoomServer = RoomServer[RoomTopic]
 
-func NewTypedRoomClient(bus psrpc.MessageBus, opts ...psrpc.ClientOption) (TypedRoomClient, error) {
-	return NewRoomClient[ParticipantTopic, RoomTopic](bus, opts...)
+func NewTypedRoomClient(params ClientParams) (TypedRoomClient, error) {
+	return NewRoomClient[RoomTopic](params.Bus, clientOptions(params)...)
 }
 
 func NewTypedRoomServer(svc RoomServerImpl, bus psrpc.MessageBus, opts ...psrpc.ServerOption) (TypedRoomServer, error) {
-	return NewRoomServer[ParticipantTopic, RoomTopic](svc, bus, opts...)
+	return NewRoomServer[RoomTopic](svc, bus, opts...)
+}
+
+//counterfeiter:generate . TypedParticipantClient
+type TypedParticipantClient = ParticipantClient[ParticipantTopic]
+type TypedParticipantServer = ParticipantServer[ParticipantTopic]
+
+func NewTypedParticipantClient(params ClientParams) (TypedParticipantClient, error) {
+	return NewParticipantClient[ParticipantTopic](params.Bus, clientOptions(params)...)
+}
+
+func NewTypedParticipantServer(svc ParticipantServerImpl, bus psrpc.MessageBus, opts ...psrpc.ServerOption) (TypedParticipantServer, error) {
+	return NewParticipantServer[ParticipantTopic](svc, bus, opts...)
 }
