f5e6e7dfcfe1e0a749c8b81009009bf5769b3460
lukasIO <mail@lukasseiler.de>
2024-02-21 10:59:51 +0100

    add index type file

---
+++ js/src/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./gen/version";
+export * from "./gen/livekit_agent_pb";
+export * from "./gen/livekit_models_pb";
+export * from "./gen/livekit_rtc_pb";
+export * from "./gen/livekit_webhook_pb";
