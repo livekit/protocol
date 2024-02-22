810b8b52ff35f0fba044a3a866d3995f63cc5862
lukasIO <mail@lukasseiler.de>
2024-02-22 14:34:30 +0100

    add egress, ingress, sip

--- packages/js/src/index.js
+++ packages/js/src/index.js
@@ -3,3 +3,6 @@ export * from "./gen/livekit_agent_pb.js";
 export * from "./gen/livekit_models_pb.js";
 export * from "./gen/livekit_rtc_pb.js";
 export * from "./gen/livekit_webhook_pb.js";
+export * from "./gen/livekit_egress.js";
+export * from "./gen/livekit_ingress.js";
+export * from "./gen/livekit_sip.js";
