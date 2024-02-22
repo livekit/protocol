6524b9b0baf2c444f24996bac9d02268414e42c0
lukasIO <mail@lukasseiler.de>
2024-02-22 14:26:18 +0100

    add other files

--- packages/js/src/index.js
+++ packages/js/src/index.js
@@ -1,3 +1,5 @@
+export * from "./gen/version.js";
+export * from "./gen/livekit_agent_pb.js";
 export * from "./gen/livekit_models_pb.js";
 export * from "./gen/livekit_rtc_pb.js";
-export * from "./gen/version.js";
+export * from "./gen/livekit_webhook_pb.js";
