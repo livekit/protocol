c26a152730de579f7bf11529080aeee212ae7ff9
lukasIO <mail@lukasseiler.de>
2024-03-11 16:05:48 +0100

    Re-export proto bigint helper

--- packages/javascript/src/index.d.ts
+++ packages/javascript/src/index.d.ts
@@ -7,3 +7,4 @@ export * from "./gen/livekit_webhook_pb.js";
 export * from "./gen/livekit_egress_pb.js";
 export * from "./gen/livekit_ingress_pb.js";
 export * from "./gen/livekit_sip_pb.js";
+export { protoInt64 } from "@bufbuild/protobuf";
--- packages/javascript/src/index.js
+++ packages/javascript/src/index.js
@@ -8,3 +8,4 @@ export * from "./gen/livekit_webhook_pb.js";
 export * from "./gen/livekit_egress_pb.js";
 export * from "./gen/livekit_ingress_pb.js";
 export * from "./gen/livekit_sip_pb.js";
+export { protoInt64 } from "@bufbuild/protobuf";
