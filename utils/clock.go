3ee5c42df6825fe7901cb85eb40ff45b7feef7d1
Lukas Herman <lherman.cs@gmail.com>
2024-04-05 14:52:16 -0400

    removed self reference and format

--- utils/clock.go
+++ utils/clock.go
@@ -15,16 +15,16 @@ type SystemClock struct{}
 
 var _ Clock = &SystemClock{}
 
-func (clock *SystemClock) Now() time.Time {
+func (*SystemClock) Now() time.Time {
 	return time.Now()
 }
 
-func (clock *SystemClock) Sleep(d time.Duration) {
+func (*SystemClock) Sleep(d time.Duration) {
 	time.Sleep(d)
 }
 
-type SimulatedClock struct{
-  clock.Mock
+type SimulatedClock struct {
+	clock.Mock
 }
 
 var _ Clock = &SimulatedClock{}
