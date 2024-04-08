1bcff7f60f4c3050d127d736a80e7fad8f7bebe2
Lukas Herman <lherman.cs@gmail.com>
2024-04-08 14:26:23 -0400

    add source

--- utils/rate.go
+++ utils/rate.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Uber Technologies, Inc.
+// Copyright (c) 2016,2020 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -17,7 +17,8 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
+//
+// SOURCE: https://github.com/uber-go/ratelimit/blob/main/limiter_mutexbased.go
 // EDIT: slight modification to allow setting rate limit on the fly
 // SCOPE: LeakyBucket
 package utils
