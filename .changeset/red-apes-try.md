9e23fdbd08859769a29ea4319a973e35ac01d0bc
Raja Subramanian <raja.gobi@tutanota.com>
2024-07-12 13:28:45 +0530

    Revert "Update to go/jose/v4 (#756)" (#759)
    This reverts commit 73ece66d30e0d7721a4ab5b9669aca603ae018ff.

--- .changeset/red-apes-try.md
+++
@@ -1,5 +0,0 @@
----
-"github.com/livekit/protocol": patch
----
-
-Update to go-jose v4
--- webhook/webhook_test.go
+++ webhook/webhook_test.go
@@ -31,7 +31,7 @@ import (
 
 const (
 	apiKey               = "mykey"
-	apiSecret            = "mysecretmysecretmysecretmysecret"
+	apiSecret            = "mysecret"
 	testAddr             = ":8765"
 	testUrl              = "http://localhost:8765"
 	webhookCheckInterval = 100 * time.Millisecond
