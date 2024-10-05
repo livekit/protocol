ec3b682c05d88583bb1615749fac1b4a0c025c07
Paul Wells <paulwe@gmail.com>
2024-10-04 22:05:46 -0700

    add zap object marshaler slice helper (#836)

---
+++ logger/zaputil/slice.go
@@ -0,0 +1,16 @@
+package zaputil
+
+import "go.uber.org/zap/zapcore"
+
+func ObjectSlice[T zapcore.ObjectMarshaler](s []T) zapcore.ArrayMarshaler {
+	return objectSlice[T](s)
+}
+
+type objectSlice[T zapcore.ObjectMarshaler] []T
+
+func (a objectSlice[T]) MarshalLogArray(e zapcore.ArrayEncoder) error {
+	for _, o := range a {
+		e.AppendObject(o)
+	}
+	return nil
+}
