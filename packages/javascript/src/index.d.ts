ad9f2862e34f9b52631b0b3f77f1d95c75365996
lukasIO <mail@lukasseiler.de>
2024-10-08 14:26:16 +0200

    Expose metrics on javascript package (#843)
    * Expose metrics on javascript package
    
    * Create gold-readers-scream.md

--- packages/javascript/src/index.d.ts
+++ packages/javascript/src/index.d.ts
@@ -7,4 +7,5 @@ export * from "./gen/livekit_webhook_pb.js";
 export * from "./gen/livekit_egress_pb.js";
 export * from "./gen/livekit_ingress_pb.js";
 export * from "./gen/livekit_sip_pb.js";
+export * from "./gen/livekit_metrics_pb.js";
 export { protoInt64 } from "@bufbuild/protobuf";
