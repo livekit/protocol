6c874ded72ede94bdc00ca74b7a02f0bdca766ae
aoife cassidy <aoife@livekit.io>
2024-11-14 01:26:08 +0200

    export cjs

--- packages/javascript/src/index.cjs
+++ packages/javascript/src/index.cjs
@@ -1,16 +1,16 @@
 // @ts-check
 const { protoInt64 } = require("@bufbuild/protobuf");
-const agentDispatch = require("./gen/cjs/livekit_agent_dispatch_pb.js");
-const agent = require("./gen/cjs/livekit_agent_pb.js");
-const egress = require("./gen/cjs/livekit_egress_pb.js");
-const ingress = require("./gen/cjs/livekit_ingress_pb.js");
-const metrics = require("./gen/cjs/livekit_metrics_pb.js");
-const models = require("./gen/cjs/livekit_models_pb.js");
-const room = require("./gen/cjs/livekit_room_pb.js");
-const rtc = require("./gen/cjs/livekit_rtc_pb.js");
-const sip = require("./gen/cjs/livekit_sip_pb.js");
-const webhook = require("./gen/cjs/livekit_webhook_pb.js");
-const version = require("./gen/cjs/version.js");
+const agentDispatch = require("./gen/cjs/livekit_agent_dispatch_pb.cjs");
+const agent = require("./gen/cjs/livekit_agent_pb.cjs");
+const egress = require("./gen/cjs/livekit_egress_pb.cjs");
+const ingress = require("./gen/cjs/livekit_ingress_pb.cjs");
+const metrics = require("./gen/cjs/livekit_metrics_pb.cjs");
+const models = require("./gen/cjs/livekit_models_pb.cjs");
+const room = require("./gen/cjs/livekit_room_pb.cjs");
+const rtc = require("./gen/cjs/livekit_rtc_pb.cjs");
+const sip = require("./gen/cjs/livekit_sip_pb.cjs");
+const webhook = require("./gen/cjs/livekit_webhook_pb.cjs");
+const version = require("./gen/cjs/version.cjs");
 
 module.exports = {
   protoInt64,
