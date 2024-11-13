da89bcb447b8fd1b11a06b2a618a954d49e6918b
aoife cassidy <aoife@livekit.io>
2024-11-13 23:47:46 +0200

    chore: support CJS
    this PR changes the CI to run two protobuf generators, one for each,
    plus another for dts. currently untested.

--- packages/javascript/src/index.js
+++ packages/javascript/src/index.js
@@ -1,13 +1,28 @@
 // @ts-check
-export { protoInt64 } from "@bufbuild/protobuf";
-export * from "./gen/livekit_agent_dispatch_pb.js";
-export * from "./gen/livekit_agent_pb.js";
-export * from "./gen/livekit_egress_pb.js";
-export * from "./gen/livekit_ingress_pb.js";
-export * from "./gen/livekit_metrics_pb.js";
-export * from "./gen/livekit_models_pb.js";
-export * from "./gen/livekit_room_pb.js";
-export * from "./gen/livekit_rtc_pb.js";
-export * from "./gen/livekit_sip_pb.js";
-export * from "./gen/livekit_webhook_pb.js";
-export * from "./gen/version.js";
+const { protoInt64 } = require("@bufbuild/protobuf");
+const agentDispatch = require("./gen/esm/livekit_agent_dispatch_pb.js");
+const agent = require("./gen/esm/livekit_agent_pb.js");
+const egress = require("./gen/esm/livekit_egress_pb.js");
+const ingress = require("./gen/esm/livekit_ingress_pb.js");
+const metrics = require("./gen/esm/livekit_metrics_pb.js");
+const models = require("./gen/esm/livekit_models_pb.js");
+const room = require("./gen/esm/livekit_room_pb.js");
+const rtc = require("./gen/esm/livekit_rtc_pb.js");
+const sip = require("./gen/esm/livekit_sip_pb.js");
+const webhook = require("./gen/esm/livekit_webhook_pb.js");
+const version = require("./gen/esm/version.js");
+
+module.exports = {
+  protoInt64,
+  ...agentDispatch,
+  ...agent,
+  ...egress,
+  ...ingress,
+  ...metrics,
+  ...models,
+  ...room,
+  ...rtc,
+  ...sip,
+  ...webhook,
+  ...version,
+}
