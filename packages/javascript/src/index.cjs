35c531927c0150026fab1de426ced4489acc63ee
aoife cassidy <aoife@livekit.io>
2024-11-18 14:44:37 +0200

    move to one dir again to fix dts errors

--- packages/javascript/src/index.cjs
+++ packages/javascript/src/index.cjs
@@ -1,16 +1,16 @@
 // @ts-check
 const { protoInt64 } = require("@bufbuild/protobuf");
-const agentDispatch = require("./gen/cjs/livekit_agent_dispatch_pb.cjs");
-const agent = require("./gen/cjs/livekit_agent_pb.cjs");
-const egress = require("./gen/cjs/livekit_egress_pb.cjs");
-const ingress = require("./gen/cjs/livekit_ingress_pb.cjs");
-const metrics = require("./gen/cjs/livekit_metrics_pb.cjs");
-const models = require("./gen/cjs/livekit_models_pb.cjs");
-const room = require("./gen/cjs/livekit_room_pb.cjs");
-const rtc = require("./gen/cjs/livekit_rtc_pb.cjs");
-const sip = require("./gen/cjs/livekit_sip_pb.cjs");
-const webhook = require("./gen/cjs/livekit_webhook_pb.cjs");
-const version = require("./gen/cjs/version.cjs");
+const agentDispatch = require("./gen/livekit_agent_dispatch_pb.cjs");
+const agent = require("./gen/livekit_agent_pb.cjs");
+const egress = require("./gen/livekit_egress_pb.cjs");
+const ingress = require("./gen/livekit_ingress_pb.cjs");
+const metrics = require("./gen/livekit_metrics_pb.cjs");
+const models = require("./gen/livekit_models_pb.cjs");
+const room = require("./gen/livekit_room_pb.cjs");
+const rtc = require("./gen/livekit_rtc_pb.cjs");
+const sip = require("./gen/livekit_sip_pb.cjs");
+const webhook = require("./gen/livekit_webhook_pb.cjs");
+const version = require("./gen/version.cjs");
 
 module.exports = {
   protoInt64,
--- packages/javascript/src/index.js
+++ packages/javascript/src/index.js
@@ -1,13 +1,13 @@
 // @ts-check
 export { protoInt64 } from "@bufbuild/protobuf";
-export * from "./gen/esm/livekit_agent_dispatch_pb.js";
-export * from "./gen/esm/livekit_agent_pb.js";
-export * from "./gen/esm/livekit_egress_pb.js";
-export * from "./gen/esm/livekit_ingress_pb.js";
-export * from "./gen/esm/livekit_metrics_pb.js";
-export * from "./gen/esm/livekit_models_pb.js";
-export * from "./gen/esm/livekit_room_pb.js";
-export * from "./gen/esm/livekit_rtc_pb.js";
-export * from "./gen/esm/livekit_sip_pb.js";
-export * from "./gen/esm/livekit_webhook_pb.js";
-export * from "./gen/esm/version.js";
+export * from "./gen/livekit_agent_dispatch_pb.js";
+export * from "./gen/livekit_agent_pb.js";
+export * from "./gen/livekit_egress_pb.js";
+export * from "./gen/livekit_ingress_pb.js";
+export * from "./gen/livekit_metrics_pb.js";
+export * from "./gen/livekit_models_pb.js";
+export * from "./gen/livekit_room_pb.js";
+export * from "./gen/livekit_rtc_pb.js";
+export * from "./gen/livekit_sip_pb.js";
+export * from "./gen/livekit_webhook_pb.js";
+export * from "./gen/version.mjs";
