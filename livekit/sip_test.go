06098b2c2952628c18000dd7807e19e2845ef319
Denys Smirnov <dennwc@pm.me>
2024-12-06 13:19:29 +0200

    Add filters to SIP list APIs.

--- livekit/sip_test.go
+++ livekit/sip_test.go
@@ -1,6 +1,7 @@
 package livekit
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -171,6 +172,168 @@ func TestSIPValidate(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			err := c.req.Validate()
 			require.Equal(t, c.exp, err == nil, "error: %v", err)
+		})
+	}
+}
+
+func TestSIPInboundTrunkFilter(t *testing.T) {
+	list := []*SIPInboundTrunkInfo{
+		0: {SipTrunkId: "A"},
+		1: {SipTrunkId: "B", Numbers: []string{"+111", "+222"}},
+		2: {SipTrunkId: "C", Numbers: []string{"+333"}},
+	}
+	cases := []struct {
+		name string
+		req  *ListSIPInboundTrunkRequest
+		exp  []*SIPInboundTrunkInfo
+	}{
+		{
+			name: "all",
+			req:  &ListSIPInboundTrunkRequest{},
+			exp:  list,
+		},
+		{
+			name: "id",
+			req: &ListSIPInboundTrunkRequest{
+				TrunkIds: []string{"B"},
+			},
+			exp: []*SIPInboundTrunkInfo{list[1]},
+		},
+		{
+			name: "ids",
+			req: &ListSIPInboundTrunkRequest{
+				TrunkIds: []string{"B", "C"},
+			},
+			exp: []*SIPInboundTrunkInfo{list[1], list[2]},
+		},
+		{
+			name: "ids order",
+			req: &ListSIPInboundTrunkRequest{
+				TrunkIds: []string{"C", "missing", "B"},
+			},
+			exp: []*SIPInboundTrunkInfo{list[2], nil, list[1]},
+		},
+		{
+			name: "numbers",
+			req: &ListSIPInboundTrunkRequest{
+				Numbers: []string{"+222"},
+			},
+			exp: []*SIPInboundTrunkInfo{list[0], list[1]},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			list := slices.Clone(list)
+			got := c.req.FilterSlice(list)
+			require.Equal(t, c.exp, got)
+		})
+	}
+}
+
+func TestSIPOutboundTrunkFilter(t *testing.T) {
+	list := []*SIPOutboundTrunkInfo{
+		0: {SipTrunkId: "A"},
+		1: {SipTrunkId: "B", Numbers: []string{"+111", "+222"}},
+		2: {SipTrunkId: "C", Numbers: []string{"+333"}},
+	}
+	cases := []struct {
+		name string
+		req  *ListSIPOutboundTrunkRequest
+		exp  []*SIPOutboundTrunkInfo
+	}{
+		{
+			name: "all",
+			req:  &ListSIPOutboundTrunkRequest{},
+			exp:  list,
+		},
+		{
+			name: "id",
+			req: &ListSIPOutboundTrunkRequest{
+				TrunkIds: []string{"B"},
+			},
+			exp: []*SIPOutboundTrunkInfo{list[1]},
+		},
+		{
+			name: "ids",
+			req: &ListSIPOutboundTrunkRequest{
+				TrunkIds: []string{"B", "C"},
+			},
+			exp: []*SIPOutboundTrunkInfo{list[1], list[2]},
+		},
+		{
+			name: "ids order",
+			req: &ListSIPOutboundTrunkRequest{
+				TrunkIds: []string{"C", "missing", "B"},
+			},
+			exp: []*SIPOutboundTrunkInfo{list[2], nil, list[1]},
+		},
+		{
+			name: "numbers",
+			req: &ListSIPOutboundTrunkRequest{
+				Numbers: []string{"+222"},
+			},
+			exp: []*SIPOutboundTrunkInfo{list[0], list[1]},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			list := slices.Clone(list)
+			got := c.req.FilterSlice(list)
+			require.Equal(t, c.exp, got)
+		})
+	}
+}
+
+func TestSIPDispatchRuleFilter(t *testing.T) {
+	list := []*SIPDispatchRuleInfo{
+		0: {SipDispatchRuleId: "A"},
+		1: {SipDispatchRuleId: "B", TrunkIds: []string{"T1", "T2"}},
+		2: {SipDispatchRuleId: "C", TrunkIds: []string{"T3"}},
+	}
+	cases := []struct {
+		name string
+		req  *ListSIPDispatchRuleRequest
+		exp  []*SIPDispatchRuleInfo
+	}{
+		{
+			name: "all",
+			req:  &ListSIPDispatchRuleRequest{},
+			exp:  list,
+		},
+		{
+			name: "id",
+			req: &ListSIPDispatchRuleRequest{
+				DispatchRuleIds: []string{"B"},
+			},
+			exp: []*SIPDispatchRuleInfo{list[1]},
+		},
+		{
+			name: "ids",
+			req: &ListSIPDispatchRuleRequest{
+				DispatchRuleIds: []string{"B", "C"},
+			},
+			exp: []*SIPDispatchRuleInfo{list[1], list[2]},
+		},
+		{
+			name: "ids order",
+			req: &ListSIPDispatchRuleRequest{
+				DispatchRuleIds: []string{"C", "missing", "B"},
+			},
+			exp: []*SIPDispatchRuleInfo{list[2], nil, list[1]},
+		},
+		{
+			name: "numbers",
+			req: &ListSIPDispatchRuleRequest{
+				TrunkIds: []string{"T2"},
+			},
+			exp: []*SIPDispatchRuleInfo{list[0], list[1]},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			list := slices.Clone(list)
+			got := c.req.FilterSlice(list)
+			require.Equal(t, c.exp, got)
 		})
 	}
 }
