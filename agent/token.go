73d48c702aa153c5b4aa44fba5e44d2347b653ba
Paul Wells <paulwe@gmail.com>
2025-05-15 06:27:25 -0700

    add agent jwt helper

--- agent/token.go
+++ agent/token.go
@@ -1,7 +1,13 @@
 package agent
 
 import (
+	"bytes"
+	"fmt"
 	"time"
+
+	"github.com/go-jose/go-jose/v3"
+	"github.com/go-jose/go-jose/v3/jwt"
+	"go.uber.org/multierr"
 
 	"github.com/livekit/protocol/auth"
 	"github.com/livekit/protocol/livekit"
@@ -34,4 +40,80 @@ func BuildAgentToken(
 		SetAttributes(participantAttributes)
 
 	return at.ToJWT()
+}
+
+type WorkerTokenConfig struct {
+	Secret  string        `yaml:"jwt_secret,omitempty"`
+	Timeout time.Duration `yaml:"jwt_timeout,omitempty"`
+}
+
+var DefaultWorkerTokenConfig = WorkerTokenConfig{
+	Timeout: 60 * time.Minute,
+}
+
+type WorkerClaims struct {
+	jwt.Claims
+}
+
+type WorkerJWT struct {
+	nodeID  string
+	keys    [][]byte
+	timeout time.Duration
+}
+
+func NewWorkerJWT(nodeID string, config WorkerTokenConfig) *WorkerJWT {
+	keys := bytes.Split([]byte(config.Secret), []byte(","))
+	for i := range keys {
+		keys[i] = bytes.TrimSpace(keys[i])
+	}
+
+	return &WorkerJWT{
+		nodeID:  nodeID,
+		keys:    keys,
+		timeout: config.Timeout,
+	}
+}
+
+func (t *WorkerJWT) Encode(claims WorkerClaims) (string, error) {
+	signer, err := jose.NewSigner(jose.SigningKey{
+		Algorithm: jose.HS256,
+		Key:       t.keys[0],
+	}, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create signer: %w", err)
+	}
+
+	now := time.Now()
+
+	claims.Issuer = t.nodeID
+	claims.Expiry = jwt.NewNumericDate(now.Add(t.timeout))
+	claims.NotBefore = jwt.NewNumericDate(now)
+	claims.IssuedAt = jwt.NewNumericDate(now)
+
+	token, err := jwt.Signed(signer).Claims(claims).CompactSerialize()
+	if err != nil {
+		return "", fmt.Errorf("failed to create signed jwt: %w", err)
+	}
+	return token, nil
+}
+
+func (t *WorkerJWT) Decode(token string) (*WorkerClaims, error) {
+	tok, err := jwt.ParseSigned(token)
+	if err != nil {
+		return nil, err
+	}
+
+	claims := &WorkerClaims{}
+	for _, k := range t.keys {
+		if erri := tok.Claims(k, &claims); erri != nil {
+			err = multierr.Append(err, erri)
+			continue
+		}
+		if erri := claims.Validate(jwt.Expected{Time: time.Now()}); erri != nil {
+			err = multierr.Append(err, erri)
+			continue
+		}
+		return claims, nil
+	}
+	return nil, err
 }
