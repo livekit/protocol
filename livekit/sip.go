09b750699c16736c144e3ca966921f637c97c8ea
Alex <alex.migolin@livekit.io>
2025-10-14 11:53:52 -0700

    Added todo

--- livekit/sip.go
+++ livekit/sip.go
@@ -393,12 +393,12 @@ func (p *SIPInboundTrunkInfo) Validate() error {
 	}
 	if err := validateHeaders(p.Headers); err != nil {
 		logger.Warnw("Header validation failed for Headers field", err)
-		// No error, just a warning for SIP RFC validation for now
+		// TODO: Once we're happy with the validation, we want this to error out
 	}
 	// Don't bother with HeadersToAttributes. If they're invalid, we just won't match
 	if err := validateHeaderNames(p.AttributesToHeaders); err != nil {
 		logger.Warnw("Header validation failed for AttributesToHeaders field", err)
-		// No error, just a warning for SIP RFC validation for now
+		// TODO: Once we're happy with the validation, we want this to error out
 	}
 	return nil
 }
@@ -489,12 +489,12 @@ func (p *SIPOutboundTrunkInfo) Validate() error {
 	}
 	if err := validateHeaders(p.Headers); err != nil {
 		logger.Warnw("Header validation failed for Headers field", err)
-		// No error, just a warning for SIP RFC validation for now
+		// TODO: Once we're happy with the validation, we want this to error out
 	}
 	// Don't bother with HeadersToAttributes. If they're invalid, we just won't match
 	if err := validateHeaderNames(p.AttributesToHeaders); err != nil {
 		logger.Warnw("Header validation failed for AttributesToHeaders field", err)
-		// No error, just a warning for SIP RFC validation for now
+		// TODO: Once we're happy with the validation, we want this to error out
 	}
 	return nil
 }
@@ -725,7 +725,7 @@ func (p *CreateSIPParticipantRequest) Validate() error {
 
 	if err := validateHeaders(p.Headers); err != nil {
 		logger.Warnw("Header validation failed for Headers field", err)
-		// No error, just a warning for SIP RFC validation for now
+		// TODO: Once we're happy with the validation, we want this to error out
 	}
 
 	// Validate display_name if provided
@@ -734,7 +734,7 @@ func (p *CreateSIPParticipantRequest) Validate() error {
 			return errors.New("display_name too long (max 128 characters)")
 		}
 
-		// TODO: Validate display name doesn't contain invalid characters
+		// TODO: Once we're happy with the validation, we want this to error out
 	}
 
 	return nil
@@ -775,7 +775,7 @@ func (p *TransferSIPParticipantRequest) Validate() error {
 
 	if err := validateHeaders(p.Headers); err != nil {
 		logger.Warnw("Header validation failed for Headers field", err)
-		// No error, just a warning for SIP RFC validation for now
+		// TODO: Once we're happy with the validation, we want this to error out
 	}
 
 	return nil
