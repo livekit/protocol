0cba9a217d877a42baf77a90e55e8105abf5be81
Anunay Maheshwari <anunay@livekit.io>
2025-11-04 04:09:43 +0530

    fix(connector): update protos for api version, errors, cleanup (#1277)
    * fix(connector): update protos for api version, errors, cleanup
    
    * pr feedback
    
    * More errors, PR feedback
    
    * generated protobuf
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- connector/errors.go
+++ connector/errors.go
@@ -17,9 +17,21 @@ package connector
 import "github.com/livekit/psrpc"
 
 var (
-	ErrMissingWhatsAppPhoneNumberId = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp phone number id is required")
-	ErrMissingWhatsAppToNumber      = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp to_phone_number is required")
-	ErrMissingWhatsAppCallId        = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp call id is required")
-	ErrMissingWhatsAppApiKey        = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp api key is required")
-	ErrIncorrectSDPType             = psrpc.NewErrorf(psrpc.InvalidArgument, "incorrect sdp type")
+	ErrMissingWhatsAppPhoneNumberId        = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp phone number id is required")
+	ErrMissingWhatsAppToNumber             = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp to_phone_number is required")
+	ErrMissingWhatsAppCallId               = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp call id is required")
+	ErrMissingWhatsAppApiKey               = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp api key is required")
+	ErrWhatsAppCallNotFound                = psrpc.NewErrorf(psrpc.NotFound, "whatsapp call not found")
+	ErrWhatsAppCloudApiVersionNotSupported = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp cloud api version not supported")
+	ErrMissingCallIDInInitiateResponse     = psrpc.NewErrorf(psrpc.MalformedResponse, "missing call id in initiate response")
+	ErrWhatsAppAPICallFailedReasonUnknown  = psrpc.NewErrorf(psrpc.Unknown, "whatsapp api call failed with unknown reason")
+
+	ErrAcceptTimeout = psrpc.NewErrorf(psrpc.DeadlineExceeded, "timeout waiting for call to be accepted")
+
+	ErrIncorrectSDPType = psrpc.NewErrorf(psrpc.InvalidArgument, "incorrect sdp type")
+	ErrSDPAnswerTimeout = psrpc.NewErrorf(psrpc.DeadlineExceeded, "timeout waiting for SDP answer")
+
+	ErrConnectionClosed  = psrpc.NewErrorf(psrpc.FailedPrecondition, "connection is closed")
+	ErrConnectorNotFound = psrpc.NewErrorf(psrpc.NotFound, "connector not found")
+	ErrServerBusy        = psrpc.NewErrorf(psrpc.ResourceExhausted, "server is busy and cannot accept new calls")
 )
