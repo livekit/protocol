0b8780ae555f39731ca9e76857f519e60b132189
Anunay Maheshwari <anunay@livekit.io>
2025-11-04 03:13:43 +0530

    More errors, PR feedback

--- connector/errors.go
+++ connector/errors.go
@@ -21,8 +21,17 @@ var (
 	ErrMissingWhatsAppToNumber             = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp to_phone_number is required")
 	ErrMissingWhatsAppCallId               = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp call id is required")
 	ErrMissingWhatsAppApiKey               = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp api key is required")
-	ErrIncorrectSDPType                    = psrpc.NewErrorf(psrpc.InvalidArgument, "incorrect sdp type")
-	ErrConnectorNotFound                   = psrpc.NewErrorf(psrpc.NotFound, "connector not found")
 	ErrWhatsAppCallNotFound                = psrpc.NewErrorf(psrpc.NotFound, "whatsapp call not found")
 	ErrWhatsAppCloudApiVersionNotSupported = psrpc.NewErrorf(psrpc.InvalidArgument, "whatsapp cloud api version not supported")
+	ErrMissingCallIDInInitiateResponse     = psrpc.NewErrorf(psrpc.MalformedResponse, "missing call id in initiate response")
+	ErrWhatsAppAPICallFailedReasonUnknown  = psrpc.NewErrorf(psrpc.Unknown, "whatsapp api call failed with unknown reason")
+
+	ErrAcceptTimeout = psrpc.NewErrorf(psrpc.DeadlineExceeded, "timeout waiting for call to be accepted")
+
+	ErrIncorrectSDPType = psrpc.NewErrorf(psrpc.InvalidArgument, "incorrect sdp type")
+	ErrSDPAnswerTimeout = psrpc.NewErrorf(psrpc.DeadlineExceeded, "timeout waiting for SDP answer")
+
+	ErrConnectionClosed  = psrpc.NewErrorf(psrpc.FailedPrecondition, "connection is closed")
+	ErrConnectorNotFound = psrpc.NewErrorf(psrpc.NotFound, "connector not found")
+	ErrServerBusy        = psrpc.NewErrorf(psrpc.ResourceExhausted, "server is busy and cannot accept new calls")
 )
