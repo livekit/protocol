ee4ff4a9d582ef21fbd7643b41eb2d20422d046a
OMAR.A <58332033+civilcoder55@users.noreply.github.com>
2026-01-05 08:17:34 +0200

    feat(sip): Add `numbers` field to SIPDispatchRuleInfo for filtering calls by called number (#1351)
    * feat(sip): Add `numbers` field to SIPDispatchRuleInfo for filtering calls by called number
    
    - Add `numbers` field to filter calls by the called number (trunk number)
    - Include `numbers` in dispatch rule priority calculation and validation
    - Update dispatch rule matching logic to check both inbound and called numbers
    - Add tests for number-specific dispatch rules and conflicts
    
    * generated protobuf
    
    * chore: add changeset
    
    * generated protobuf
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- livekit/livekit_sip.pb.go
+++ livekit/livekit_sip.pb.go
@@ -3057,8 +3057,10 @@ type SIPDispatchRuleInfo struct {
 	Rule              *SIPDispatchRule       `protobuf:"bytes,2,opt,name=rule,proto3" json:"rule,omitempty"`
 	TrunkIds          []string               `protobuf:"bytes,3,rep,name=trunk_ids,json=trunkIds,proto3" json:"trunk_ids,omitempty"`
 	HidePhoneNumber   bool                   `protobuf:"varint,4,opt,name=hide_phone_number,json=hidePhoneNumber,proto3" json:"hide_phone_number,omitempty"`
-	// Dispatch Rule will only accept a call made to these numbers (if set).
+	// Dispatch Rule will only accept a call made from these numbers (if set).
 	InboundNumbers []string `protobuf:"bytes,7,rep,name=inbound_numbers,json=inboundNumbers,proto3" json:"inbound_numbers,omitempty"`
+	// Dispatch Rule will only accept a call made to these numbers (if set).
+	Numbers []string `protobuf:"bytes,13,rep,name=numbers,proto3" json:"numbers,omitempty"`
 	// Human-readable name for the Dispatch Rule.
 	Name string `protobuf:"bytes,5,opt,name=name,proto3" json:"name,omitempty"`
 	// User-defined metadata for the Dispatch Rule.
@@ -3072,7 +3074,7 @@ type SIPDispatchRuleInfo struct {
 	// RoomConfiguration to use if the participant initiates the room
 	RoomConfig      *RoomConfiguration `protobuf:"bytes,10,opt,name=room_config,json=roomConfig,proto3" json:"room_config,omitempty"`
 	KrispEnabled    bool               `protobuf:"varint,11,opt,name=krisp_enabled,json=krispEnabled,proto3" json:"krisp_enabled,omitempty"`
-	MediaEncryption SIPMediaEncryption `protobuf:"varint,12,opt,name=media_encryption,json=mediaEncryption,proto3,enum=livekit.SIPMediaEncryption" json:"media_encryption,omitempty"` // NEXT ID: 13
+	MediaEncryption SIPMediaEncryption `protobuf:"varint,12,opt,name=media_encryption,json=mediaEncryption,proto3,enum=livekit.SIPMediaEncryption" json:"media_encryption,omitempty"` // NEXT ID: 14
 	unknownFields   protoimpl.UnknownFields
 	sizeCache       protoimpl.SizeCache
 }
@@ -3138,6 +3140,13 @@ func (x *SIPDispatchRuleInfo) GetHidePhoneNumber() bool {
 func (x *SIPDispatchRuleInfo) GetInboundNumbers() []string {
 	if x != nil {
 		return x.InboundNumbers
+	}
+	return nil
+}
+
+func (x *SIPDispatchRuleInfo) GetNumbers() []string {
+	if x != nil {
+		return x.Numbers
 	}
 	return nil
 }
@@ -4644,13 +4653,14 @@ const file_livekit_sip_proto_rawDesc = "" +
 	"\x14sip_dispatch_rule_id\x18\x01 \x01(\tR\x11sipDispatchRuleId\x128\n" +
 	"\areplace\x18\x02 \x01(\v2\x1c.livekit.SIPDispatchRuleInfoH\x00R\areplace\x128\n" +
 	"\x06update\x18\x03 \x01(\v2\x1e.livekit.SIPDispatchRuleUpdateH\x00R\x06updateB\b\n" +
-	"\x06action\"\xbe\x05\n" +
+	"\x06action\"\xd8\x05\n" +
 	"\x13SIPDispatchRuleInfo\x12/\n" +
 	"\x14sip_dispatch_rule_id\x18\x01 \x01(\tR\x11sipDispatchRuleId\x12,\n" +
 	"\x04rule\x18\x02 \x01(\v2\x18.livekit.SIPDispatchRuleR\x04rule\x12\x1b\n" +
 	"\ttrunk_ids\x18\x03 \x03(\tR\btrunkIds\x12*\n" +
 	"\x11hide_phone_number\x18\x04 \x01(\bR\x0fhidePhoneNumber\x12'\n" +
-	"\x0finbound_numbers\x18\a \x03(\tR\x0einboundNumbers\x12\x12\n" +
+	"\x0finbound_numbers\x18\a \x03(\tR\x0einboundNumbers\x12\x18\n" +
+	"\anumbers\x18\r \x03(\tR\anumbers\x12\x12\n" +
 	"\x04name\x18\x05 \x01(\tR\x04name\x12B\n" +
 	"\bmetadata\x18\x06 \x01(\tB&\x88\xec,\x01\x92\xec,\x1e<redacted ({{ .Size }} bytes)>R\bmetadata\x12t\n" +
 	"\n" +
