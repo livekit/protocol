b409f47e4af99be48965f08466bf1e6fcb10f70d
Paul Wells <paulwe@gmail.com>
2026-02-23 11:14:05 -0800

    remove cloud only methods from reporter interface (#1423)
    * remove cloud only methods from reporter interface
    
    * Create clever-gifts-live.md

---
+++ .changeset/clever-gifts-live.md
@@ -0,0 +1,5 @@
+---
+"@livekit/protocol": patch
+---
+
+remove cloud only methods from reporter interface
--- observability/reporter.go
+++ observability/reporter.go
@@ -1,6 +1,9 @@
 package observability
 
 import (
+	"github.com/go-logr/logr"
+
+	"github.com/livekit/protocol/logger"
 	"github.com/livekit/protocol/observability/agentsobs"
 	"github.com/livekit/protocol/observability/egressobs"
 	"github.com/livekit/protocol/observability/gatewayobs"
@@ -13,12 +16,14 @@ import (
 
 const Project = "livekit"
 
+var discardLogger = logger.LogRLogger(logr.Discard())
+
 type Reporter interface {
+	Logger(projectID string) (logger.Logger, error)
 	Room() roomobs.Reporter
 	Agent() agentsobs.Reporter
 	Gateway() gatewayobs.Reporter
 	Telephony() telephonyobs.Reporter
-	Connector() any // any is a placeholder for the connector type
 	Egress() egressobs.Reporter
 	Ingress() ingressobs.Reporter
 	GatewayMetrics() any // any is a placeholder for the gateway metrics type
@@ -32,6 +37,10 @@ func NewReporter() Reporter {
 }
 
 type reporter struct{}
+
+func (reporter) Logger(projectID string) (logger.Logger, error) {
+	return discardLogger, nil
+}
 
 func (reporter) Room() roomobs.Reporter {
 	return roomobs.NewNoopReporter()
@@ -47,10 +56,6 @@ func (reporter) Gateway() gatewayobs.Reporter {
 
 func (reporter) Telephony() telephonyobs.Reporter {
 	return telephonyobs.NewNoopReporter()
-}
-
-func (reporter) Connector() any {
-	return nil
 }
 
 func (reporter) Egress() egressobs.Reporter {
