fed965f8e0c783b1839f993b94b4d5eda3f2597f
Paul Wells <paulwe@gmail.com>
2026-05-05 13:22:55 -0700

    rename agent environment to deployment

--- agent/environment.go
+++ agent/environment.go
@@ -2,22 +2,22 @@ package agent
 
 import "fmt"
 
-const MaxEnvironmentLength = 64
+const MaxDeploymentLength = 64
 
-func ValidateEnvironment(env string) error {
-	if env == "" {
+func ValidateDeployment(deployment string) error {
+	if deployment == "" {
 		return nil
 	}
-	if len(env) > MaxEnvironmentLength {
-		return fmt.Errorf("environment exceeds %d bytes", MaxEnvironmentLength)
+	if len(deployment) > MaxDeploymentLength {
+		return fmt.Errorf("deployment exceeds %d bytes", MaxDeploymentLength)
 	}
-	for i := 0; i < len(env); i++ {
-		c := env[i]
+	for i := 0; i < len(deployment); i++ {
+		c := deployment[i]
 		switch {
 		case c == '_':
-			return fmt.Errorf("environment contains reserved character %q", c)
+			return fmt.Errorf("deployment contains reserved character %q", c)
 		case c <= ' ' || c == 0x7f:
-			return fmt.Errorf("environment contains whitespace or control byte at position %d", i)
+			return fmt.Errorf("deployment contains whitespace or control byte at position %d", i)
 		}
 	}
 	return nil
