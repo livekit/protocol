8068a685608203a6df6d4d5deda6aef23544795b
Paul Wells <paulwe@gmail.com>
2026-05-06 20:52:33 -0700

    reproduce comments

--- utils/protojson/protojson.go
+++ utils/protojson/protojson.go
@@ -1,3 +1,7 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package protojson
 
 import (
@@ -8,6 +12,11 @@ import (
 type MarshalOptions = protojson.MarshalOptions
 type UnmarshalOptions = protojson.UnmarshalOptions
 
+// Format formats the message as a multiline string.
+// This function is only intended for human consumption and ignores errors.
+// Do not depend on the output being stable. Its output will change across
+// different builds of your program, even when using the same version of the
+// protobuf module.
 func Format(m proto.Message) string {
 	return MarshalOptions{
 		Multiline:    true,
@@ -15,12 +24,18 @@ func Format(m proto.Message) string {
 	}.Format(m)
 }
 
+// Marshal writes the given [proto.Message] in JSON format using default options.
+// Do not depend on the output being stable. Its output will change across
+// different builds of your program, even when using the same version of the
+// protobuf module.
 func Marshal(m proto.Message) ([]byte, error) {
 	return MarshalOptions{
 		AllowPartial: true,
 	}.Marshal(m)
 }
 
+// Unmarshal reads the given []byte into the given [proto.Message].
+// The provided message must be mutable (e.g., a non-nil pointer to a message).
 func Unmarshal(b []byte, m proto.Message) error {
 	return UnmarshalOptions{
 		AllowPartial:   true,
