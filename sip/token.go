26a30b2ece65f27afc3e4044755ee5f656efc3a9
cnderrauber <zengjiestc@gmail.com>
2026-08-07 21:48:11 +0800

    Revert "Add RoomAdmin to SIPTokenParams (#1677)"
    This reverts commit 51ee3aaa1f0483f460797326e9523f313fd55cb5.

--- sip/token.go
+++ sip/token.go
@@ -31,7 +31,6 @@ type SIPTokenParams struct {
 	ParticipantAttributes map[string]string
 	RoomPreset            string
 	RoomConfig            *livekit.RoomConfiguration
-	RoomAdmin             bool
 }
 
 func BuildSIPToken(params SIPTokenParams) (string, error) {
@@ -39,7 +38,6 @@ func BuildSIPToken(params SIPTokenParams) (string, error) {
 	at := auth.NewAccessToken(params.APIKey, params.APISecret).
 		SetVideoGrant(&auth.VideoGrant{
 			RoomJoin:             true,
-			RoomAdmin:            params.RoomAdmin,
 			Room:                 params.RoomName,
 			CanSubscribe:         &t,
 			CanPublish:           &t,
