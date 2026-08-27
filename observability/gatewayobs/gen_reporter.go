18f0a1b6197437005ac2374602ddde5f7968a517
Russell Martin <russell.martin@livekit.io>
2026-08-27 15:58:28 +0100

    Add realtime cached image reporter
    Expose the generated reporter API needed to persist discounted realtime image-cache usage.

--- observability/gatewayobs/gen_reporter.go
+++ observability/gatewayobs/gen_reporter.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-const Version_F2D2A50 = true
+const Version_3V1GTG0 = true
 
 type KeyResolver interface {
 	Resolve(string)
@@ -118,6 +118,7 @@ type modelReporter interface {
 	ReportRealtimeOutputAudioTokens(v uint64)
 	ReportRealtimeCachedTextTokens(v uint64)
 	ReportRealtimeCachedAudioTokens(v uint64)
+	ReportRealtimeCachedImageTokens(v uint64)
 	ReportRealtimeTextInputMessages(v uint64)
 	ReportSttDuration(v uint32)
 	ReportTtsChars(v uint32)
--- observability/gatewayobs/gen_reporter_noop.go
+++ observability/gatewayobs/gen_reporter_noop.go
@@ -173,6 +173,7 @@ func (r *noopModelReporter) ReportRealtimeOutputTextTokens(v uint64)            
 func (r *noopModelReporter) ReportRealtimeOutputAudioTokens(v uint64)             {}
 func (r *noopModelReporter) ReportRealtimeCachedTextTokens(v uint64)              {}
 func (r *noopModelReporter) ReportRealtimeCachedAudioTokens(v uint64)             {}
+func (r *noopModelReporter) ReportRealtimeCachedImageTokens(v uint64)             {}
 func (r *noopModelReporter) ReportRealtimeTextInputMessages(v uint64)             {}
 func (r *noopModelReporter) ReportSttDuration(v uint32)                           {}
 func (r *noopModelReporter) ReportTtsChars(v uint32)                              {}
@@ -203,6 +204,7 @@ func (t *noopModelTx) ReportRealtimeOutputTextTokens(v uint64)              {}
 func (t *noopModelTx) ReportRealtimeOutputAudioTokens(v uint64)             {}
 func (t *noopModelTx) ReportRealtimeCachedTextTokens(v uint64)              {}
 func (t *noopModelTx) ReportRealtimeCachedAudioTokens(v uint64)             {}
+func (t *noopModelTx) ReportRealtimeCachedImageTokens(v uint64)             {}
 func (t *noopModelTx) ReportRealtimeTextInputMessages(v uint64)             {}
 func (t *noopModelTx) ReportSttDuration(v uint32)                           {}
 func (t *noopModelTx) ReportTtsChars(v uint32)                              {}
