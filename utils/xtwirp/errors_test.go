72d9ef18b13e8a8d04e4e27d4585f28e9687a776
Genseric <58644045+genseric-ghiro@users.noreply.github.com>
2026-09-22 15:24:07 -0400

    Removing twirp error unknown prefix (#1814)

--- utils/xtwirp/errors_test.go
+++ utils/xtwirp/errors_test.go
@@ -10,6 +10,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/livekit/protocol/utils/xtwirp"
+	"github.com/livekit/psrpc"
 )
 
 func TestStatus(t *testing.T) {
@@ -24,4 +25,33 @@ func TestStatus(t *testing.T) {
 	require.True(t, ok)
 	require.Equal(t, st, got)
 	require.Equal(t, st.Details(), got.Details())
+}
+
+// A non-twirp error is first wrapped as twirp.Unknown, then re-coded once the
+// gRPC status is known. The re-coding must not fold the "twirp error unknown:"
+// prefix into the message.
+func TestToErrorKeepsMessage(t *testing.T) {
+	cases := []struct {
+		name string
+		err  error
+		msg  string
+	}{
+		{
+			name: "psrpc",
+			err:  psrpc.NewErrorFromResponse(string(psrpc.NotFound), "object cannot be found"),
+			msg:  "object cannot be found",
+		},
+		{
+			name: "grpc status",
+			err:  status.Error(codes.NotFound, "object cannot be found"),
+			msg:  "rpc error: code = NotFound desc = object cannot be found",
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			e := xtwirp.ToError(c.err)
+			require.Equal(t, twirp.NotFound, e.Code())
+			require.Equal(t, c.msg, e.Msg())
+		})
+	}
 }
